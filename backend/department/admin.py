ca802e4607402064d9f8d3db8db8f9d09e24e91e
Ankan Poddar <friendyankan@gmail.com>
2018-02-01 16:06:16 +0530

    Register faculty and research models in admin

--- backend/department/admin.py
+++ backend/department/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from department.models import Department
+from department.models import *
 
 # Register your models here.
+
+class FacultyModelAdmin(admin.ModelAdmin):
+    list_display = ['__str__', '_department']
+
+class ResearchModelAdmin(admin.ModelAdmin):
+    list_display = ['__str__', '_department', '_institute_involved', '_faculty_involved']
+
 admin.site.register(Department)
+admin.site.register(Faculty, FacultyModelAdmin)
+admin.site.register(Research, ResearchModelAdmin)
--- backend/department/models.py
+++ backend/department/models.py
@@ -2,39 +2,72 @@ from django.db import models
 import datetime
 import uuid
 
+
 class Department(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	name=models.CharField(max_length=255)
-	short_code=models.CharField(max_length=4)
-	about_us=models.TextField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name=models.CharField(max_length=255)
+    short_code=models.CharField(max_length=4)
+    about_us=models.TextField()
+
+    def __str__(self):
+        return self.name
+
 
 class Faculty(models.Model):
-	id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	name=models.CharField(max_length=255)
-	research_interest=models.TextField()
-	email=models.CharField(max_length=255,default="")
-	mobile=models.IntegerField()
-	joining_year=models.DateField(auto_now_add=True)
-	department=models.ForeignKey(Department,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Faculties'
+
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name=models.CharField(max_length=255)
+    research_interest=models.TextField()
+    email=models.CharField(max_length=255, default="")
+    mobile=models.CharField(max_length=10, null=True)
+    joining_year=models.CharField(max_length=4, null=True)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def _department(self):
+        return self.department.name
+
 
 class Research(models.Model):
-	id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	department=models.ForeignKey(Department,on_delete=models.CASCADE)
-	collab_inst=models.TextField()
-	area=models.CharField(max_length=255)
-	faculty_involved=models.TextField()
-	date=models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Research'
+
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    collab_inst=models.TextField()
+    area=models.CharField(max_length=255)
+    faculty_involved=models.TextField()
+    date=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.area
+
+    def _institute_involved(self):
+        return self.collab_inst
+
+    def _department(self):
+        return self.department.name
+
+    def _faculty_involved(self):
+        return self.faculty_involved
+
 
 class Electives(models.Model):
-	id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-	short_code=models.CharField(max_length=7) #for example if an elective is MME 461 total size is 7 including the space
-	title=models.CharField(max_length=200)
-	is_open=models.BooleanField(default=True)
-	department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    short_code=models.CharField(max_length=7) #for example if an elective is MME 461 total size is 7 including the space
+    title=models.CharField(max_length=200)
+    is_open=models.BooleanField(default=True)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
 
 class FacultyRoles(models.Model):
-	department=models.ForeignKey(Department,on_delete=models.CASCADE)
-	faculty=models.ForeignKey(Faculty,on_delete=models.CASCADE)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    faculty=models.ForeignKey(Faculty,on_delete=models.CASCADE)
     #role=models.ForeignKey(Roles,on_delete=models.CASCADE)
 
 # class Roles(models.Model):
