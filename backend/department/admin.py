858580add0ec782d1fde39483b181a6c21988156
Devansh Goenka <devanshgoenka97@gmail.com>
2018-02-02 00:02:24 +0530

    [FEAT] Fixed and tested all Department Models4

--- backend/department/admin.py
+++ backend/department/admin.py
@@ -9,6 +9,50 @@ class FacultyModelAdmin(admin.ModelAdmin):
 class ResearchModelAdmin(admin.ModelAdmin):
     list_display = ['__str__', '_department', '_institute_involved', '_faculty_involved']
 
+class ProjectModelAdmin(admin.ModelAdmin):
+	list_display = ['__str__', '_department', '_institute_involved', '_faculty_involved', '_funding']
+
+class RolesModelAdmin(admin.ModelAdmin):
+	list_display = ['__str__']
+
+class FacultyRolesModelAdmin(admin.ModelAdmin):
+	list_display = ['_faculty', '_role']
+
+class ActitvitesModelAdmin(admin.ModelAdmin):
+	list_display = ['_department', '_speakers', '_programme', '_date']
+
+class DegreeModelAdmin(admin.ModelAdmin):
+	list_display = ['__str__', '_description']
+
+class ProgrammeModelAdmin(admin.ModelAdmin):
+	list_display = ['_title', '_degree', '_department']
+
+class CoursesModelAdmin(admin.ModelAdmin):
+	list_display = ['__str__', '_short_code', '_semester', '_course_type']
+
+class FacilitiesModelAdmin(admin.ModelAdmin):
+	list_display = ['_department', '_name', '_category']
+
+class ElectivesModelAdmin(admin.ModelAdmin):
+	list_display = ['_department', '_title', '_is_open', '_semester']
+
+class DepartmentPhotosModelAdmin(admin.ModelAdmin):
+	list_display = ['_department', '_title', '_date']
+
+class DepartmentNewsModelAdmin(admin.ModelAdmin):
+	list_display = ['_department', '_title', '_date']
+
 admin.site.register(Department)
 admin.site.register(Faculty, FacultyModelAdmin)
 admin.site.register(Research, ResearchModelAdmin)
+admin.site.register(Project, ProjectModelAdmin)
+admin.site.register(Roles, RolesModelAdmin)
+admin.site.register(FacultyRoles, FacultyRolesModelAdmin)
+admin.site.register(Activites, ActitvitesModelAdmin)
+admin.site.register(Degree, DegreeModelAdmin)
+admin.site.register(Programme, ProgrammeModelAdmin)
+admin.site.register(Courses, CoursesModelAdmin)
+admin.site.register(Facilities, FacilitiesModelAdmin)
+admin.site.register(Electives, ElectivesModelAdmin)
+admin.site.register(DepartmentNews, DepartmentNewsModelAdmin)
+admin.site.register(DepartmentPhotos, DepartmentPhotosModelAdmin)
--- backend/department/models.py
+++ backend/department/models.py
@@ -23,7 +23,7 @@ class Faculty(BaseModel):
     name = models.CharField(max_length=255)
     research_interest = models.TextField()
     email = models.CharField(max_length=255, default="")
-    mobile = models.BigIntegerField(max_length=10, null=True)
+    mobile = models.BigIntegerField(null=True)
     joining_year = models.CharField(max_length=4, null=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
@@ -81,57 +81,185 @@ class Project(BaseModel):
         return self.funding
 
 class Roles(BaseModel):
+    class Meta:
+        verbose_name_plural = 'Roles'
+
     name = models.CharField(max_length=56)
 
+    def __str__(self):
+        return self.name
+
 class FacultyRoles(BaseModel):
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Faculty Roles'
+
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     role = models.ForeignKey(Roles, on_delete=models.CASCADE)
 
+    def _faculty(self):
+        return self.faculty.name
+
+    def _role(self):
+        return self.role.name
+
 class Activites(BaseModel):
+
+    class Meta:
+        verbose_name_plural = 'Activites'
+
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     speakers = models.CharField(max_length=512)
     programme = models.TextField()
     date = models.DateField(auto_now_add=True)
 
+    def _department(self):
+        return self.department.name
+
+    def _speakers(self):
+        return self.speakers
+
+    def _programme(self):
+        return self.programme
+
+    def _date(self):
+        return self.date
+
 class Degree(BaseModel):
+
+    class Meta:
+        verbose_name_plural = 'Degree'
+
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=512)
 
+    def __str__(self):
+        return self.name
+
+    def _description(self):
+        return self.description
+
 class Programme(BaseModel):
+
+    class Meta:
+        verbose_name_plural = 'Programmes'
+
     title = models.CharField(max_length=255)
     degree = models.ForeignKey(Degree, on_delete=models.CASCADE)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
+    def _title(self):
+        return self.title
+
+    def _degree(self):
+        return self.degree.name
+
+    def _department(self):
+        return self.department.name
+
 class Courses(BaseModel):
-    COURSE_TYPES = (('1','L'), ('2','T'), ('3','S'))
+
+    class Meta:
+        verbose_name_plural = 'Courses'
+
+    COURSE_TYPES = (('L','Lecture'), ('T','Tutorial'), ('S','Sessional'))
     programme = models.ForeignKey(Programme, on_delete=models.CASCADE)
     short_code = models.CharField(max_length=7)
     semester = models.IntegerField()
-    course_type = models.IntegerField(choices=COURSE_TYPES, default=1)
+    course_type = models.CharField(choices=COURSE_TYPES, default='L', max_length=30)
     credits = models.IntegerField()
 
+    def __str__(self):
+        return self.programme.title
+
+    def _short_code(self):
+        return self.short_code
+
+    def _semester(self):
+        return self.semester
+
+    def _course_type(self):
+        return self.course_type
+
 class Facilities(BaseModel):
+
+    class Meta:
+        verbose_name_plural = 'Facilities'
+
     FACILITY_CHOICES = (('1','Laboratory'), ('2','Equipment'))
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     category = models.IntegerField(choices=FACILITY_CHOICES, default=1)
 
+    def _department(self):
+        return self.department.name
+
+    def _name(self):
+        return self.name
+
+    def _category(self):
+        return self.category
+
 class Electives(BaseModel):
+
+    class Meta:
+        verbose_name_plural = 'Electives'
+
     short_code = models.CharField(max_length=7)
     title = models.CharField(max_length=200)
     is_open = models.BooleanField(default=True)
     semester = models.IntegerField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
+    def _department(self):
+        return self.department.name
+
+    def _title(self):
+        return self.title
+
+    def _semester(self):
+        return self.semester
+
+    def _is_open(self):
+        return self.is_open
+
 class DepartmentPhotos(BaseModel):
+
+    class Meta:
+        verbose_name_plural = 'Department Photos'
+
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     date = models.DateField(auto_now_add=True)
     link = models.CharField(max_length=255)
 
+    def _department(self):
+        return self.department.name
+
+    def _title(self):
+        return self.title
+
+    def _date(self):
+        return self.date
+
 class DepartmentNews(BaseModel):
+
+    class Meta:
+        verbose_name_plural = 'Department News'
+
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
+
+    def _department(self):
+        return self.department.name
+
+    def _title(self):
+        return self.title
+
+    def _date(self):
+        return self.date
