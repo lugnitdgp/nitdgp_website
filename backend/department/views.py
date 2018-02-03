5bcd89efe182cb41edd7ecfdcfee0d55e1104816
Ankan Poddar <friendyankan@gmail.com>
2018-02-03 16:38:07 +0530

    [FEAT] Add HOD and AboutUs serializer

--- backend/department/views.py
+++ backend/department/views.py
@@ -2,14 +2,19 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
 from department.serializers import *
+from department.models import *
+
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 class DepartmentListViewSet(ListAPIView):
 
     queryset = Department.objects.all()
     serializer_class = DepartmentListSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (AllowAny, )
 
     def list(self, request, *args, **kwargs):
 
@@ -22,10 +27,10 @@ class FacultyViewSet(ListAPIView):
 
     queryset = Faculty.objects.all()
     serializer_class = FacultySerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (AllowAny, )
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset().filter(department__short_code__iexact=args[0])
+    def list(self, request, slug):
+        queryset = self.get_queryset().filter(department__short_code__iexact=slug)
         serializer = FacultySerializer(queryset, many=True)
         return Response({"faculties":serializer.data})
 
@@ -35,4 +40,39 @@ class DepartmentViewSet(RetrieveAPIView):
     queryset = Department.objects.all()
     serializer_class = MainSerializer
 
+    def list(self, request, slug):
+    	queryset = self.get_queryset().filter(department__short_code__iexact=slug)
+    	serializer = FacultySerializer(queryset, many=True)
+    	return Response({"faculties": serializer.data})
 
+
+class AboutUsViewSet(RetrieveAPIView):
+
+    queryset = Department.objects.all()
+    serializer_class = AboutUsSerializer
+    permission_classes = (AllowAny, )
+
+    def retrieve(self, request, id):
+        try:
+            filter_kwargs = {'id': id}
+            queryset = get_object_or_404(self.queryset, **filter_kwargs)
+            serializer = AboutUsSerializer(queryset)
+            return Response({"about-us": serializer.data})
+        except:
+            raise Http404
+
+
+class HodViewSet(RetrieveAPIView):
+
+    queryset = Faculty.objects.all()
+    serializer_class = HodSerializer
+    permission_classes = (AllowAny, )
+
+    def retrieve(self, request, id):
+        try:
+            filter_kwargs = {'department_id' : id}
+            queryset = get_object_or_404(self.queryset, **filter_kwargs)
+            serializer = HodSerializer(queryset)
+            return Response({"hod": serializer.data})
+        except:
+            raise Http404
