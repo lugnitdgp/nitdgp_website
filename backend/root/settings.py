e41ce2f7e72b955ccd4d76085595c5a4331f5ada
Divyanshu Mehta <divyanshumehta@outlook.com>
2018-02-05 16:24:04 +0530

    [CHANGE]Page size in settings.py

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -118,7 +118,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 30,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
