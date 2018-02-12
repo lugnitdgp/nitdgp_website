5bb7ae7a162b705bd9a0cfcb32cd76a386b25251
Devansh Goenka <devanshgoenka97@gmail.com>
2018-02-13 01:43:25 +0530

    [FEAT] Add app for Information and Acitvities

--- backend/facilities/urls.py
+++ backend/facilities/urls.py
@@ -6,8 +6,8 @@ app_name = 'facilities'
 
 urlpatterns = [
 
-	url(r'^facilities/library$', LibraryViewSet.as_view(), name='view-library'),
-	url(r'^facilities/eresource$', EResourceViewSet.as_view(), name='view-eresource'),
-	url(r'^facilities/sac$', SACViewSet.as_view(), name='view-sac'),
+    url(r'^facilities/library$', LibraryViewSet.as_view(), name='view-library'),
+    url(r'^facilities/eresource$', EResourceViewSet.as_view(), name='view-eresource'),
+    url(r'^facilities/sac$', SACViewSet.as_view(), name='view-sac'),
 
-	]
+    ]
---
+++ backend/information/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from information.views import *
+
+app_name = 'information'
+
+urlpatterns = [
+
+
+    ]
