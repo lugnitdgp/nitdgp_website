a8b91acf0ac15d54178edfa5b8e966fef43d0358
Ankan Poddar <friendyankan@gmail.com>
2018-03-22 00:16:11 +0530

    Add the initial vue.js setup with vue cli

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import "bootstrap";
-
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById('app'));
