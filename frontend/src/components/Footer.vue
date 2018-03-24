90914b231f060ad80a39948685b03a12c9f5a911
Ankan Poddar <friendyankan@gmail.com>
2018-03-24 15:20:37 +0530

    Add all the images to static folder and clean up Footer.vue

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,68 +1,67 @@
 <template>
-<footer class="page-footer center-on-small-only" >
-  <div class="container" id="footer-container" >
-    <div class="container-fluid" align="center" >
-      <div class="row" style="background-color: #ffffff;padding:5px;border-radius: 10px;">
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" src="http://172.16.20.3/nitdgp_website/static/img/govlogo1.png"></a>
+  <footer class="page-footer center-on-small-only" >
+    <div class="container" id="footer-container" >
+      <div class="container-fluid" align="center" >
+        <div class="row">
+          <div v-for="logo in logos" class="container-fluid logo-container">
+            <a :href="logo.href"><img :src="['/static/img/' + logo.filename]"></a>
+          </div>
         </div>
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" src="http://172.16.20.3/nitdgp_website/static/img/govlogo2.png"></a>
-        </div>
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" src="http://172.16.20.3/nitdgp_website/static/img/govlogo3.png"></a>
-        </div>
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" src="http://172.16.20.3/nitdgp_website/static/img/govlogo4.png"></a>
-        </div>
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" src="http://172.16.20.3/nitdgp_website/static/img/govlogo5.png"></a>
-        </div>
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" style="padding-top: 10%" src="http://172.16.20.3/nitdgp_website/static/img/govlogo6.png"></a>
-        </div>
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" src="http://172.16.20.3/nitdgp_website/static/img/govlogo7.png"></a>
-        </div>
-        <div class="container-fluid logo-container">
-          <a href="#!"><img id="footer-logos" src="http://172.16.20.3/nitdgp_website/static/img/govlogo8.png"></a>
-        </div>
-      </div>
-      <hr class="style4">
-      <div class="row quick-links-row">
-        <div class="container-fluid quick-links-container">
-          <a class="waves-effect waves-light" href="#!"> <span>Holidays</span></a>
-        </div>
-        <div class="container-fluid quick-links-container">
-          <a href="#!"> <span>Archives</span></a>
-        </div>
-        <div class="container-fluid quick-links-container">
-          <a href="#!"> <span>Policies</span></a>
-        </div>
-        <div class="container-fluid quick-links-container">
-          <a href="#!"> <span>Webteam</span></a>
+        <hr class="style4">
+        <div class="row quick-links-row">
+          <div v-for="link in links" class="container-fluid quick-links-container">
+            <a :href="link.href">{{ link.name }}</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="footer-copyright" id="footer-copyright" >
-    <div class="row">
-      <div class="container-fluid" >
-        <span class="copyright-txt" >Last updated: 1/12/18 12:12 PM </span>
-      </div>
-      <div class="container-fluid" >
-        <span class="copyright-txt">© 2018 Copyright: <a href="https://www.nitdgp.ac.in"> nitdgp.ac.in </a></span>
-      </div>
-      <div class="container-fluid" >
-        <span class="copyright-txt">Visitors Count: 12312 </span>
+    <div class="footer-copyright" id="footer-copyright" >
+      <div class="row">
+        <div class="container-fluid" >
+          <span class="copyright-txt" >Last updated: {{ last_updated }} </span>
+        </div>
+        <div class="container-fluid" >
+          <span class="copyright-txt">© 2018 Copyright: <a href="https://www.nitdgp.ac.in"> nitdgp.ac.in </a></span>
+        </div>
+        <div class="container-fluid" >
+          <span class="copyright-txt">Visitors Count: {{ visitors }} </span>
+        </div>
       </div>
     </div>
-  </div>
-</footer>
+  </footer>
 </template>
 
 <script>
 export default {
-    name: 'Footer'
+    name: 'Footer',
+    data() {
+      return {
+        visitors: 12312,
+        last_updated: "1/12/18 12:12 PM",
+        logos: [{name: 'govlogo1', filename: 'govlogo1.png', href:"#!"},
+                {name: 'govlogo2', filename: 'govlogo2.png', href:"#!"},
+                {name: 'govlogo3', filename: 'govlogo3.png', href:"#!"},
+                {name: 'govlogo4', filename: 'govlogo4.png', href:"#!"},
+                {name: 'govlogo5', filename: 'govlogo5.png', href:"#!"},
+                {name: 'govlogo6', filename: 'govlogo6.png', href:"#!"},
+                {name: 'govlogo7', filename: 'govlogo7.png', href:"#!"},
+                {name: 'govlogo8', filename: 'govlogo8.png', href:"#!"}],
+        links: [{name: "Holidays", href:"#!"},
+                {name: "Archives", href:"#!"},
+                {name: "Policies", href:"#!"},
+                {name: "Webteam", href:"#!"}]
+      }
+    }
 }
 </script>
+
+<style scoped>
+  #footer-container > .container-fluid > .row:first-child {
+    background-color: #fff;
+    padding:5px;
+    border-radius: 10px;
+  }
+  .logo-container:nth-child(6) a img {
+    padding-top: 10%;
+  }
+</style>
