7ac12a9e4c2817ca7cb0112c550e3b58120efee1
Ankan Poddar <friendyankan@gmail.com>
2018-03-24 14:58:01 +0530

    Put comments in the homepage component for better readability

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,14 +2,18 @@
   <div>
     <div class="page-content-container l0">
       <div class="all-tiles">
-        <div v-for="(row,index) in results" class="row big-row">
+        <!-- Big row of sections -->
+        <div v-for="row in results" class="row big-row">
+          <!-- A section -->
           <div v-for="section in row" class="col big-col">
             <div :class="['card card-cascade narrower card-' + section.priority]">
               <div class="view gradient-card-header tile-title">
                   <p class="tile-title-text">{{ section.section_name }}</p>
               </div>
               <div class="card-body text-center">
+                <!-- A row of tiles -->
                 <div v-for="tile_row in section.contents" class="row">
+                  <!-- A tile -->
                   <div v-for="tile in tile_row" class="col tile-small">
                     <a :href="tile.link">
                       <div align="center" class="tile-content">
@@ -18,11 +22,15 @@
                       </div>
                     </a>
                   </div>
+                  <!-- End of a tile -->
                 </div>
+                <!-- End of a row of tiles -->
               </div>
             </div>
           </div>
+          <!-- End of a section -->
         </div>
+        <!-- End of big row of sections -->
       </div>
     </div>
   </div>
@@ -43,33 +51,36 @@ export default {
     axios.get('http://172.16.20.3:8000/dashboard')
     .then(response => {
       this.results = response.data.results;
-      let x = 0, y = 0;
-      let rows = [[]]
+      let x = 0, y = 0
+      let section_rows = [[]]
+      // Loop over all the sections
       this.results.map((element, index) => {
-        let prev = 0;
-        let r = 0;
-        let tiles = [];
-        element.contents.map((tile, tile_index) => {
-          if (prev != tile.row) {
-            ++prev, r = 0;
-            tiles[prev-1] = [];
+        let row = 0, col = 0
+        let tiles_rows = []
+        // Loop over the tiles of a section
+        element.contents.map((tile) => {
+          // Go to the next row when a new tile row value has been encountered.
+          // Reset the col value and initialize the new row with an empty list.
+          if (row != tile.row) {
+            ++row, col = 0
+            tiles_rows[row-1] = []
           }
-          tiles[prev-1][r] = tile;
-          r++;
-        });
-        element.contents = tiles;
-        rows[x][y] = element;
-        y++;
+          tiles_rows[row-1][col++] = tile
+        })
+        element.contents = tiles_rows
+        section_rows[x][y++] = element
+        // Go to next row after 3 elements. Increment the x value, reset the y
+        // value and initialize the next row with an empty list.
         if ((index+1) % 3 == 0) {
-          x++, y = 0;
-          rows[x] = [];
+          x++, y = 0
+          section_rows[x] = []
         }
-      });
-      this.results = rows;
-      console.log(this.results);
+      })
+      this.results = section_rows
     })
     .catch(e => {
       this.errors.push(e)
+      console.log(errors)
     })
   }
 }
