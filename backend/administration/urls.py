ca8f385c1e3b8097c80b9dab5466cb2afee05570
Compro-Prasad <comproprasad@gmail.com>
2018-03-27 07:19:57 +0530

    [FIX] urls in urls.py should always have a trailing slash
    But when urls were registered without a trailing slash one gets a 404
    when accessing with a trailing slash.
    
    This is resolved by including a trailing slash in all the urls. This
    prevents error even when accessing urls without a trailing slash.

--- backend/administration/urls.py
+++ backend/administration/urls.py
@@ -6,9 +6,9 @@ app_name = 'administration'
 
 urlpatterns = [
 
-    url(r'^administration/director$', DirectorViewSet.as_view(), name='view-director'),
-    url(r'^administration/chairperson$', ChairpersonViewSet.as_view(), name='view-chairperson'),
-    url(r'^administration/registrar$', RegistrarViewSet.as_view(), name='view-registrar'),
-    url(r'^administration/bog$', BOGViewSet.as_view(), name='view-bog')
+    url(r'^administration/director/$', DirectorViewSet.as_view(), name='view-director'),
+    url(r'^administration/chairperson/$', ChairpersonViewSet.as_view(), name='view-chairperson'),
+    url(r'^administration/registrar/$', RegistrarViewSet.as_view(), name='view-registrar'),
+    url(r'^administration/bog/$', BOGViewSet.as_view(), name='view-bog')
 
 ]
