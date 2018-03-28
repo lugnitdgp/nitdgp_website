9e04f6e2351b51c985d903880db36f5ac43420c6
Ankan Poddar <friendyankan@gmail.com>
2018-03-28 15:09:11 +0530

    Change the loader

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div class="loader" v-show="loader">
-      <span class="loader-dot"></span>
+      <span class="loader-dot-one"></span>
+      <span class="loader-dot-two"></span>
     </div>
     <Header/>
     <router-view @hideloader="hideLoader" />
@@ -47,35 +48,80 @@ export default {
     z-index:1050;
   }
 
-  .loader-dot {
+  .loader-dot-one {
     position: absolute;
-    display: inline-block;
-    top: 48%;
-    height: 10px;
-    left: 50%;
-    width: 0px;
-    background-color: #f00;
+    background-image: url("/static/img/nitdgp_logo.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    top: calc(50% - 125px);
+    height: 150px;
+    width: 100%;
+    animation-name: grow-n-shrink;
+    animation-duration: 1.5s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-out;
+    animation-delay: 0.1s;
+  }
+
+  .loader-dot-two {
+    position: absolute;
+    top: calc(50% + 75px);
+    width: 50%;
+    left: 25%;
+    border-bottom: 6px solid #215863;
+    animation-name: move;
+    animation-duration: 1.5s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-out;
+    animation-delay: 0.1s;
+  }
+
+  @keyframes grow-n-shrink {
+    0% {
+      transform: scale(1);
+    }
+    10% {
+      transform: scale(1.05);
+    }
+    20% {
+      transform: scale(1.1);
+    }
+    30% {
+      transform: scale(1.18);
+    }
+    40% {
+      transform: scale(1.25);
+    }
+    50% {
+      transform: scale(1.20);
+    }
+    60% {
+      transform: scale(1.15);
+    }
+    70% {
+      transform: scale(1.20);
+    }
+    80% {
+      transform: scale(1.28);
+    }
+    90% {
+      transform: scale(1.15);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 
   @keyframes move {
     0% {
-      width: 0px;
-      left: 50%;
+      transform: scaleX(0);
     }
     50% {
-      width: 100%;
-      left: 0%;
+      transform: scaleX(1);
     }
     100% {
-      width: 0px;
-      left: 50%;
+      transform: scaleX(0);
     }
-  }
-  .loader-dot {
-    animation-name: move;
-    animation-duration: 1s;
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-out;
-    animation-delay: 0.1s;
   }
 </style>
