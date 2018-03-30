af6d69c7b12bc94c812f0bd490465138fd46a8d7
Arnav <arnav.kr.grv@gmail.com>
2018-03-30 14:41:54 +0530

    [FEAT] Finished adding pages for facilities group

---
+++ frontend/src/components/facilities/Cif.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="l1 page-type-links faci">
+		<!-- CONTENT -->
+		<div class="card">
+		  <a class="card-header white-text">Central Instruments Facilities</a>
+		  <div class="card-body">
+		    <div class="img-wrap">
+		      <img src="http://nitdgp.ac.in/images/xray.jpg" width="100%">
+		    </div>
+		    <div>
+		      <div align="justify">
+		        <font>
+		        <b> XRD Equipment:</b> PANalytical XPert Pro Basic X-ray Powder Diffraction Package with all accessories and soft wares. Offers Defect studies of Metallic Alloys including Rietveld Analsis, Phase Analysis, Chemical Analysis for both inorganic and organic specimen, Microstructural Parameters. Compatible for samples of solid flat specimens, solid powders, liquid specimen, thin films etc.
+		        </font><br>
+		        <font><br>
+		          <b>SEM with EDS Equipment: SEM: </b>S-3000N, HITACHI-JAPAN, EDS : THERMO-NORAN-USA, SIX-NSS 200 with PC Monitor, Softwares etc. Offers morphological and surface studies of Metallic Alloys, semi-conducting, insulating and biological samples. Chemical analysis, Phase analysis, X-ray mapping etc. on the EDS.
+		        </font>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'Cif',
+		created () {
+			this.$emit('hideloader', true)
+		}
+	}
+</script>
