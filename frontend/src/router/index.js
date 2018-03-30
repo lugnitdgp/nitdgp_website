5bdd0717d4707fdc91cd7f52c78b60f5c1cbb16b
Arnav <arnav.kr.grv@gmail.com>
2018-03-30 14:43:16 +0530

    Added routes for pgs in facilities group

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,17 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Reports from '@/components/information/Reports'
 import Library from '@/components/facilities/Library'
+import Mu from '@/components/facilities/Mu'
+import Ceam from '@/components/facilities/Ceam'
+import Cif from '@/components/facilities/Cif'
+import Hostels from '@/components/facilities/Hostels'
+import Po from '@/components/facilities/Po'
+import Banks from '@/components/facilities/Banks'
+import Guest from '@/components/facilities/Guest'
+import Sac from '@/components/facilities/Sac'
+import Hvl from '@/components/facilities/Hvl'
+import Canteen from '@/components/facilities/Canteen'
+import Cc from '@/components/facilities/Cc'
 import Admission from '@/components/academics/Admission'
 import Calendar from '@/components/academics/Calendar'
 import Registrations from '@/components/academics/Registrations'
@@ -30,6 +41,61 @@ export default new Router({
       path: '/facilities/library',
       name: 'Library',
       component: Library
+    },
+    {
+      path: '/facilities/hvl',
+      name: 'Hvl',
+      component: Hvl
+    },
+    {
+      path: '/facilities/canteen',
+      name: 'Canteen',
+      component: Canteen
+    },
+    {
+      path: '/facilities/hostels',
+      name: 'Hostels',
+      component: Hostels
+    },
+    {
+      path: '/facilities/banks',
+      name: 'Banks',
+      component: Banks
+    },
+    {
+      path: '/facilities/cc',
+      name: 'Cc',
+      component: Cc
+    },
+    {
+      path: '/facilities/mu',
+      name: 'Mu',
+      component: Mu
+    },
+    {
+      path: '/facilities/ceam',
+      name: 'Ceam',
+      component: Ceam
+    },
+    {
+      path: '/facilities/cif',
+      name: 'Cif',
+      component: Cif
+    },
+    {
+      path: '/facilities/guest',
+      name: 'Guest',
+      component: Guest
+    },
+    {
+      path: '/facilities/sac',
+      name: 'Sac',
+      component: Sac
+    },
+    {
+      path: '/facilities/po',
+      name: 'Po',
+      component: Po
     },
     {
       path: '/academics/admission',
