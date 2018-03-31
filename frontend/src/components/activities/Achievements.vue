86a0799aba37bff1add24eee9ea52a99447aba8d
Compro-Prasad <comproprasad@gmail.com>
2018-03-31 13:38:30 +0530

    Some activities pages are rendered by data instead of static content
    	modified:   frontend/src/components/activities/Achievements.vue
    	modified:   frontend/src/components/activities/Events.vue

--- frontend/src/components/activities/Achievements.vue
+++ frontend/src/components/activities/Achievements.vue
@@ -1,30 +1,50 @@
 <template>
-  <div class="l1 page-type-links">
-    <!-- CONTENT -->
-    <div class="card">
-      <a class="card-header white-text">Achievements, Honours &amp; Recognitions </a>
-      <div class="card-body">
-	<div class="card-text">
-	  <h1>May include a photo carousel here</h1>
-	  <ul>
-	    <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Achievement detail here</a></li>
-	    <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	      Achievement detail here</a></li>
-	    <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	      Achievement detail here</a></li>
-	    <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	      Achievement detail here</a></li>
-	  </ul>
-	</div>
-      </div>
-    </div>
-  </div>
+  <links-page>
+    <card title="Achievements, Honours and Recognitions">
+      <h1>May include a photo carousel here</h1>
+      <ul>
+	<li v-for="ach in achievements">
+          <a :href="ach.file" target="new">{{ ach.title }}</a>
+        </li>
+      </ul>
+    </card>
+  </links-page>
 </template>
+
 <script>
+import Card from '@/components/Card.vue'
+import LinksPage from '@/components/LinksPage.vue'
+
 export default {
   name: 'Achievements',
+  data () {
+    return {
+      achievements: [
+        {
+          title: "Achievement detail here",
+          file: "http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg"
+        },
+        {
+          title: "Achievement detail here",
+          file: "http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg"
+        },
+        {
+          title: "Achievement detail here",
+          file: "http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf"
+        },
+        {
+          title: "Achievement detail here",
+          file: "http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf"
+        }
+      ]
+    }
+  },
   created () {
     this.$emit('hideloader', true)
+  },
+  components: {
+    Card,
+    LinksPage
   }
 }
 </script>
