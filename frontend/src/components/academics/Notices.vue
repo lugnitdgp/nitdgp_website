82897c84ed12d6aaf0fdc6fec8c0bea514ae5c41
Ankan Poddar <friendyankan@gmail.com>
2018-04-04 13:20:19 +0530

    WIP for #28. Notices.vue dynamically loads notices from backend

--- frontend/src/components/academics/Notices.vue
+++ frontend/src/components/academics/Notices.vue
@@ -3,175 +3,147 @@
     <div class="card">
       <a class="card-header white-text">Notices</a>
       <div class="card-body">
-	<!-- Nav tabs -->
-	<ul class="nav nav-tabs nav-justified not-tab">
-	  <li class="nav-item">
-	    <a class="nav-link active" data-toggle="tab" href="#panel1" role="tab">Academic</a>
-	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link" data-toggle="tab" href="#panel2" role="tab">Student</a>
-	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link" data-toggle="tab" href="#panel3" role="tab">General</a>
-	  </li>
-	</ul>
-	<!-- Tab panels -->
-	<div class="tab-content card">
-	  <!--Panel 1-->
-	  <div class="tab-pane fade in show active" id="panel1" role="tabpanel">
-	    <ul>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	    </ul>
-	    <div>
-	      <ul class="pagination justify-content-center">
-	        <li class="page-item">
-	          <a class="page-link" aria-label="Previous">
-	            <span aria-hidden="true">&laquo;</span>
-	            <span class="sr-only">Previous</span>
-	          </a>
-	        </li>
+      	<!-- Nav tabs -->
+      	<ul class="nav nav-tabs nav-justified not-tab">
+      	  <li class="nav-item">
+      	    <a class="nav-link active" data-toggle="tab" href="#panel1" role="tab">Academic</a>
+      	  </li>
+      	  <li class="nav-item">
+      	    <a class="nav-link" data-toggle="tab" href="#panel2" role="tab">Student</a>
+      	  </li>
+      	  <li class="nav-item">
+      	    <a class="nav-link" data-toggle="tab" href="#panel3" role="tab">General</a>
+      	  </li>
+      	</ul>
+      	<!-- Tab panels -->
+      	<div class="tab-content card">
+      	  <!--Panel 1-->
+      	  <div class="tab-pane fade in show active" id="panel1" role="tabpanel">
+      	    <ul v-if="academic">
+      	      <li v-for="notice in academic">
+                <a :href="notice.file" target="new">{{ notice.title }}</a>
+              </li>
+      	    </ul>
+      	    <div>
+      	      <ul v-if="academic" class="pagination justify-content-center">
+      	        <li class="page-item">
+      	          <a class="page-link" aria-label="Previous">
+      	            <span aria-hidden="true">&laquo;</span>
+      	            <span class="sr-only">Previous</span>
+      	          </a>
+      	        </li>
 
-	        <li class="page-item active" ><a class="page-link" data-toggle="tab" href="#li1" role="tab">1</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li2" role="tab">2</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li3" role="tab">3</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li4" role="tab">4</a></li>
-	        <li class="page-item">
-	          <a class="page-link" aria-label="Next">
-	            <span aria-hidden="true">&raquo;</span>
-	            <span class="sr-only">Next</span>
-	          </a>
-	        </li>
-	      </ul>
-	    </div>
-	  </div>
-	  <!--/.Panel 1-->
-	  <!--Panel 2-->
-	  <div class="tab-pane fade" id="panel2" role="tabpanel">
-	    <ul>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	    </ul>
-	    <div>
-	      <ul class="pagination justify-content-center">
-	        <li class="page-item">
-	          <a class="page-link" aria-label="Previous">
-	            <span aria-hidden="true">&laquo;</span>
-	            <span class="sr-only">Previous</span>
-	          </a>
-	        </li>
+      	        <li v-for="i in parseInt(academic.length/10 + 1)"
+                    class="'page-item ' + i == 1 : 'active' ? ''" >
+                      <a class="page-link" data-toggle="tab"
+                         href="'#li' + i" role="tab">{{i}}</a>
+                </li>
 
-	        <li class="page-item active" ><a class="page-link" data-toggle="tab" href="#li1" role="tab">1</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li2" role="tab">2</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li3" role="tab">3</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li4" role="tab">4</a></li>
-	        <li class="page-item">
-	          <a class="page-link" aria-label="Next">
-	            <span aria-hidden="true">&raquo;</span>
-	            <span class="sr-only">Next</span>
-	          </a>
-	        </li>
-	      </ul>
-	    </div>
-	  </div>
-	  <!--/.Panel 2-->
-	  <!--Panel 3-->
-	  <div class="tab-pane fade" id="panel3" role="tabpanel">
-	    <br>
-	    <ul>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_application_form.jpg" target="new">Application For Duplicates/ Transcripts/ Migration Certificate</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/transcript_notice/transcript_fee_slip.jpg" target="new">
-	        Transcript Fee Slip</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/Verification%20of%20Education%20Qualification..pdf" target="new">
-	        Verification of Education Qualification</a></li>
-	      <li><a href="http://nitdgp.ac.in/all_pdf17/ISSUANCE%20OF%20ACADEMIC%20DOCUMENTS..pdf" target="new">
-	        Issuance of Academic Documents</a></li>
-	    </ul>
-	    <div>
-	      <ul class="pagination justify-content-center">
-	        <li class="page-item">
-	          <a class="page-link" aria-label="Previous">
-	            <span aria-hidden="true">&laquo;</span>
-	            <span class="sr-only">Previous</span>
-	          </a>
-	        </li>
+      	        <li class="page-item">
+      	          <a class="page-link" aria-label="Next">
+      	            <span aria-hidden="true">&raquo;</span>
+      	            <span class="sr-only">Next</span>
+      	          </a>
+      	        </li>
+      	      </ul>
+      	    </div>
+      	  </div>
+      	  <!--/.Panel 1-->
+      	  <!--Panel 2-->
+      	  <div class="tab-pane fade" id="panel2" role="tabpanel">
+            <ul v-if="student">
+              <li v-for="notice in student">
+                <a :href="notice.file" target="new">{{ notice.title }}</a>
+              </li>
+            </ul>
+      	    <div>
+      	      <ul  v-if="student" class="pagination justify-content-center">
+      	        <li class="page-item">
+      	          <a class="page-link" aria-label="Previous">
+      	            <span aria-hidden="true">&laquo;</span>
+      	            <span class="sr-only">Previous</span>
+      	          </a>
+      	        </li>
 
-	        <li class="page-item active" ><a class="page-link" data-toggle="tab" href="#li1" role="tab">1</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li2" role="tab">2</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li3" role="tab">3</a></li>
-	        <li class="page-item"><a class="page-link" data-toggle="tab" href="#li4" role="tab">4</a></li>
-	        <li class="page-item">
-	          <a class="page-link" aria-label="Next">
-	            <span aria-hidden="true">&raquo;</span>
-	            <span class="sr-only">Next</span>
-	          </a>
-	        </li>
-	      </ul>
-	    </div>
-	  </div>
-	  <!--/.Panel 3-->
-	</div>
+                <li v-for="i in parseInt(student.length/10 + 1)"
+                    class="'page-item ' + i == 1 : 'active' ? ''" >
+                      <a class="page-link" data-toggle="tab"
+                         href="'#li' + i" role="tab">{{i}}</a>
+                </li>
+
+      	        <li class="page-item">
+      	          <a class="page-link" aria-label="Next">
+      	            <span aria-hidden="true">&raquo;</span>
+      	            <span class="sr-only">Next</span>
+      	          </a>
+      	        </li>
+      	      </ul>
+      	    </div>
+      	  </div>
+      	  <!--/.Panel 2-->
+      	  <!--Panel 3-->
+      	  <div class="tab-pane fade" id="panel3" role="tabpanel">
+      	    <br>
+            <ul v-if="general">
+              <li v-for="notice in general">
+                <a :href="notice.file" target="new">{{ notice.title }}</a>
+              </li>
+            </ul>
+      	    <div>
+      	      <ul v-if="general" class="pagination justify-content-center">
+      	        <li class="page-item">
+      	          <a class="page-link" aria-label="Previous">
+      	            <span aria-hidden="true">&laquo;</span>
+      	            <span class="sr-only">Previous</span>
+      	          </a>
+      	        </li>
+
+                <li v-for="i in parseInt(general.length/10 + 1)"
+                    class="'page-item ' + i == 1 : 'active' ? ''" >
+                      <a class="page-link" data-toggle="tab"
+                         href="'#li' + i" role="tab">{{i}}</a>
+                </li>
+
+      	        <li class="page-item">
+      	          <a class="page-link" aria-label="Next">
+      	            <span aria-hidden="true">&raquo;</span>
+      	            <span class="sr-only">Next</span>
+      	          </a>
+      	        </li>
+      	      </ul>
+      	    </div>
+      	  </div>
+      	  <!--/.Panel 3-->
+      	</div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
+import axios from 'axios'
+import { genBackendURL } from '@/common.js'
+
 export default {
   name: 'Notices',
+  data() {
+    return {
+      academic: [],
+      student: [],
+      general: []
+    }
+  },
   created () {
-    this.$emit('hideloader', true)
-  }
+    axios.get(genBackendURL('academics/notices'))
+         .then(response => {
+           this.academic = response.data.notices.academic
+           this.student = response.data.notices.student
+           this.general = response.data.notices.general
+           this.$emit('hideloader', true)
+         })
+         .catch(e => {
+           console.log(e)
+         })
+  },
 }
 </script>
