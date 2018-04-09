f8fcdd9ab7e478b4a168bde93f70d5628716dda2
Ankan Poddar <friendyankan@gmail.com>
2018-04-09 22:53:55 +0530

    Make the director component dynamic

--- frontend/src/components/administration/Director.vue
+++ frontend/src/components/administration/Director.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="page-type-person">
-    <h1 class="name-title" align="center"> MEET OUR DIRECTOR </h1>
+    <h1 class="name-title" align="center">MEET OUR DIRECTOR</h1>
     <div class="card testimonial-card">
       <div class="card-up">
-        <img src="/static/img/director_image.jpg" >
+        <img :src="director.image" >
       </div>
       <div class="card-body">
         <h3 class="mt-1 white-text">
-          <strong>Prof. Anupam Basu</strong>
+          <strong>{{director.name}}</strong>
         </h3>
         <h4 class="desig cyan-text">Director</h4>
         <hr class="hr-grey">
@@ -18,20 +18,38 @@
           Durgapur, West Bengal, India,<br>
           PIN – 713209.<br>
           <i class="fa fa-envelope fa-1x"></i><br>
-          director@admin.nitdgp.ac.in<br>
+          {{ director.email }}<br>
           <i class="fa fa-phone fa-1x"></i><br>
-          Phone :+91 343 2546397 (O)<br>
-          Fax :+91 343 2547375<br>
-          <a href="#" class="white-text">*MoreInfo*</a>
+          Phone :+91 {{ director.mobile }} (O)<br>
+          <!-- Fax :+91 343 2547375<br> -->
+          <!-- <a href="#" class="white-text">*MoreInfo*</a> -->
         </p>
       </div>
     </div>
   </div>
 </template>
+
 <script>
+import axios from 'axios'
+import { genBackendURL } from '@/common.js'
+
 export default {
   name: 'Director',
+  data () {
+    return {
+      director: {}
+    }
+  },
   created () {
+    axios.get(genBackendURL("administration/director"))
+         .then(response => {
+           this.director = response.data.director
+           this.director.mobile = String(this.director.mobile).slice(0,3) +
+                                  " " + String(this.director.mobile).slice(3,)
+         })
+         .catch(e => {
+           console.log(e)
+         })
     this.$emit('hideloader', true)
   }
 }
