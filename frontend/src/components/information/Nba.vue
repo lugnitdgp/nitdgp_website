1254e6048351d33672ab35559f663f4c1792ff38
Compro-Prasad <comproprasad@gmail.com>
2018-04-09 18:17:40 +0530

    [FEATURE] Added dynamic load functionality for NBA

--- frontend/src/components/information/Nba.vue
+++ frontend/src/components/information/Nba.vue
@@ -3,7 +3,7 @@
     <card title="National Board of Accreditation">
       <ul>
         <li v-for="nba in nbas">
-          <a target="new" :href="nba.link">
+          <a target="new" :href="nba.file">
             {{ nba.title }}
           </a>
         </li>
@@ -15,21 +15,25 @@
 <script>
 import Card from "@/components/Card"
 import LinksPage from "@/components/LinksPage"
+import axios from 'axios'
+import { genBackendURL } from '@/common.js'
 
 export default {
   name: "Nba",
   data () {
     return {
-      nbas: [
-        {
-          link: "http://http//nitdgp.ac.in/information/NBA%20Data.doc",
-          title: "NBA Accreditation Data"
-        }
-      ]
+      nbas: []
     }
   },
   created () {
-    this.$emit('hideloader', true)
+    axios.get(genBackendURL('information/nba'))
+         .then(response => {
+           this.nbas = response.data.results
+           this.$emit('hideloader', true)
+         })
+         .catch(e => {
+           console.log("Axios(GET[information]): Error: " + e)
+         })
   },
   components: {
     Card,
