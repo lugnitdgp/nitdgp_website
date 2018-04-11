46dd248af244dd53b5caf20a7553e345725cd475
Divyanshu Mehta <divyanshumehta@outlook.com>
2018-04-11 10:21:51 +0530

    Added dynamic component for Senate

--- frontend/src/components/administration/Senate.vue
+++ frontend/src/components/administration/Senate.vue
@@ -1,13 +1,41 @@
 <template>
-	<h1>
-		<a href="http://www.nitdgp.ac.in/information/senate_meetings.php">Link to the old web</a>
-	</h1>
+  <links-page>
+    <card title="Senate">
+      <ul>
+        <li v-for="meeting in meetings">
+          <a :href="meeting.file">{{ meeting.title }}</a>
+        </li>
+      </ul>
+    </card>
+  </links-page>
 </template>
+
 <script>
+import axios from 'axios'
+import LinksPage from '@/components/LinksPage'
+import Card from '@/components/Card'
+import { genBackendURL } from '@/common.js'
+
 export default {
-  name: 'Senate',
+  name: "Senate",
+  data () {
+    return {
+      meetings: {}
+    }
+  },
   created () {
-    this.$emit('hideloader', true)
+    axios.get(genBackendURL("administration/senate"))
+         .then(response => {
+           this.meetings = response.data.results
+           this.$emit('hideloader', true)
+         })
+         .catch(e => {
+           console.log(e)
+         })
+  },
+  components: {
+    LinksPage,
+    Card
   }
 }
 </script>
