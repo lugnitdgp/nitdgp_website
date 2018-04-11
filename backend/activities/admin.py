f7dd3ebce439497852520b6f8ea702ab8cbbcfaa
Devansh Goenka <devanshgoenka97@gmail.com>
2018-04-11 15:26:07 +0530

    [FEAT] Add models for Seminar and Student Clubs

--- backend/activities/admin.py
+++ backend/activities/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from activities.models import *
 
-# Register your models here.
+
+class StudentClubModelAdmin(admin.ModelAdmin):
+	list_display = ['_name', '_description', '_link']
+
+
+class SeminarEventModelAdmin(admin.ModelAdmin):
+	list_display = ['_title', '_file', '_date']
+
+admin.site.register(StudentClub, StudentClubModelAdmin)
+admin.site.register(SeminarEvent, SeminarEventModelAdmin)
--- backend/activities/models.py
+++ backend/activities/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from base.models import BaseModel
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class StudentClub(BaseModel):
+
+	class Meta:
+		verbose_name_plural = 'Student Clubs and Technical Activities'
+		ordering = ('name', )
+
+	name = models.CharField(max_length=512)
+	image = models.ImageField(upload_to='activities/student_clubs/')
+	description = RichTextField()
+	link = models.URLField()
+
+	def _name(self):
+		return self.name
+
+	def _description(self):
+		return self.description
+
+	def _link(self):
+		return self.link
+
+
+class SeminarEvent(BaseModel):
+
+	class Meta:
+		verbose_name_plural = 'Seminar and Events'
+		ordering = ('-date', )
+
+	title = models.CharField(max_length=512)
+	file = models.FileField(upload_to='activities/seminar_and_events/%Y/%m/%d')
+	date = models.DateField()
+
+	def _title(self):
+		return self.title
+
+	def _file(self):
+		return self.file
+
+	def _date(self):
+		return self.date
