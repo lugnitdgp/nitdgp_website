8439f606f7133801603c35559e6784d7a2361534
Compro-Prasad <comproprasad@gmail.com>
2018-04-12 13:37:31 +0530

    [FIXED] single row problem. Now Deans show up on multiple rows

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -39,11 +39,36 @@ function genBackendURL (str) {
 }
 
 
+function range(start, stop, step) {
+  if (typeof stop == 'undefined') {
+    // one param defined
+    stop = start;
+    start = 0;
+  }
+
+  if (typeof step == 'undefined') {
+    step = 1;
+  }
+
+  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+    return [];
+  }
+
+  var result = [];
+  for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+
 export {
   backURL,
   baseURL,
   prettify,
   randomValue,
   getUniqueId,
-  genBackendURL
+  genBackendURL,
+  range
 }
--- frontend/src/components/administration/Deans.vue
+++ frontend/src/components/administration/Deans.vue
@@ -3,15 +3,15 @@
     <card title="Deans">
       <div class="carousel-inner person-list" role="listbox">
         <div class="carousel-item active">
-          <div class="row">
-            <div class="col" v-for="dean in deans">
-              <card-testimonial :image="dean.image" :name="dean.name"
-                :desig="dean.role + ' ' + dean.designation">
+          <div class="row" v-for="i in range(1, deans.length+1, 6)">
+            <div class="col" v-for="j in range(i, i+6 < (deans.length+1) ? i+6 : (deans.length+1))">
+              <card-testimonial :image="deans[j-1].image" :name="deans[j-1].name"
+                :desig="deans[j-1].role + ' ' + deans[j-1].designation">
                 <i class="fa fa-envelope"></i><br>
-                <strong>{{ dean.email }}</strong><br>
+                <strong>{{ deans[j-1].email }}</strong><br>
                 <i class="fa fa-address-book"></i><br>
-                <strong>+91-343-{{ dean.phone }}</strong></br>
-                <strong>+91-{{ dean.alternate_phone }}</strong></br>
+                <strong>+91-343-{{ deans[j-1].phone }}</strong></br>
+                <strong>+91-{{ deans[j-1].alternate_phone }}</strong></br>
               </card-testimonial>
             </div>
           </div>
@@ -26,7 +26,7 @@ import axios from 'axios'
 import LinksPage from '@/components/LinksPage'
 import Card from '@/components/Card'
 import CardTestimonial from '@/components/CardTestimonial'
-import { genBackendURL } from '@/common.js'
+import { range, genBackendURL } from '@/common.js'
 
 export default {
   name: "Deans",
@@ -36,6 +36,7 @@ export default {
     }
   },
   created () {
+    console.log(range(3))
     axios.get(genBackendURL("administration/deans"))
          .then(response => {
            this.deans = response.data.results
@@ -44,6 +45,9 @@ export default {
          .catch(e => {
            console.log(e)
          })
+  },
+  methods: {
+    range: range
   },
   components: {
     LinksPage,
