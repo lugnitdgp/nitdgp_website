d0eb86c821bce3d1bcf6cba17930e0de7cccf250
Compro-Prasad <comproprasad@gmail.com>
2018-04-14 02:48:49 +0530

    Now watch poll works for webpack dev server
    Which was kind of optional and didn’t work previously for me

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -18,9 +18,9 @@ module.exports = {
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
-    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    poll: 1000, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
