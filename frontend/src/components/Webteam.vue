a4b8292db5631938e7a6678f7ce92ccfd4c9ea1e
Divyanshu Mehta <divyanshumehta@outlook.com>
2018-04-16 16:57:24 +0530

    Added WebTeam component

--- frontend/src/components/Webteam.vue
+++ frontend/src/components/Webteam.vue
@@ -1,150 +1,91 @@
 <template>
-	<div class="carousel-inner person-list" role="listbox">
-      <h1 style="text-align: center;"><strong>-- WEBTEAM --</strong></h1>
-      <div class="carousel-item active">
-        <div class="row">
-          <div class="col">
-            <div class="card testimonial-card">
-              <div class="card-up">
-              </div>
-              <div class="avatar mx-auto">
-                <img src="/static/img/hodcse.jpg" class="rounded-circle img-responsive">
-              </div>
-              <div class="card-body-in">
-                <h4 class="card-title mt-1"><strong>Dr. Sanyal Gautam</strong></h4>
-                <hr>
-                <p class="min-profile">
-                  <strong>-- Dean (Faculty Welfare) --</strong><br>
-                  <i class="fa fa-envelope"></i><br>
-                  <strong>gautam.sanyal@cse.nitdgp.ac.in<br>nitgsanyal@gmail.com</strong><br>
-                  <i class="fa fa-address-book"></i><br>
-                  <strong>+91-9434788006</strong></br>
-                  <strong>+91-8465478456</strong></br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card testimonial-card">
-              <div class="card-up">
-              </div>
-              <div class="avatar mx-auto">
-                <img src="/static/img/hodcse.jpg" class="rounded-circle img-responsive">
-              </div>
-              <div class="card-body-in">
-                <h4 class="card-title mt-1"><strong>Dr. Sanyal Gautam</strong></h4>
-                <hr>
-                <p class="min-profile">
-                  <strong>-- Dean (Faculty Welfare) --</strong><br>
-                  <i class="fa fa-envelope"></i><br>
-                  <strong>gautam.sanyal@cse.nitdgp.ac.in<br>nitgsanyal@gmail.com</strong><br>
-                  <i class="fa fa-address-book"></i><br>
-                  <strong>+91-9434788006</strong></br>
-                  <strong>+91-8465478456</strong></br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card testimonial-card">
-              <div class="card-up">
-              </div>
-              <div class="avatar mx-auto">
-                <img src="/static/img/hodcse.jpg" class="rounded-circle img-responsive">
-              </div>
-              <div class="card-body-in">
-                <h4 class="card-title mt-1"><strong>Dr. Sanyal Gautam</strong></h4>
-                <hr>
-                <p class="min-profile">
-                  <strong>-- Dean (Faculty Welfare) --</strong><br>
-                  <i class="fa fa-envelope"></i><br>
-                  <strong>gautam.sanyal@cse.nitdgp.ac.in<br>nitgsanyal@gmail.com</strong><br>
-                  <i class="fa fa-address-book"></i><br>
-                  <strong>+91-9434788006</strong></br>
-                  <strong>+91-8465478456</strong></br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card testimonial-card">
-              <div class="card-up">
-              </div>
-              <div class="avatar mx-auto">
-                <img src="/static/img/hodcse.jpg" class="rounded-circle img-responsive">
-              </div>
-              <div class="card-body-in">
-                <h4 class="card-title mt-1"><strong>Dr. Sanyal Gautam</strong></h4>
-                <hr>
-                <p class="min-profile">
-                  <strong>-- Dean (Faculty Welfare) --</strong><br>
-                  <i class="fa fa-envelope"></i><br>
-                  <strong>gautam.sanyal@cse.nitdgp.ac.in<br>nitgsanyal@gmail.com</strong><br>
-                  <i class="fa fa-address-book"></i><br>
-                  <strong>+91-9434788006</strong></br>
-                  <strong>+91-8465478456</strong></br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card testimonial-card">
-              <div class="card-up">
-              </div>
-              <div class="avatar mx-auto">
-                <img src="/static/img/hodcse.jpg" class="rounded-circle img-responsive">
-              </div>
-              <div class="card-body-in">
-                <h4 class="card-title mt-1"><strong>Dr. Sanyal Gautam</strong></h4>
-                <hr>
-                <p class="min-profile">
-                  <strong>-- Dean (Faculty Welfare) --</strong><br>
-                  <i class="fa fa-envelope"></i><br>
-                  <strong>gautam.sanyal@cse.nitdgp.ac.in<br>nitgsanyal@gmail.com</strong><br>
-                  <i class="fa fa-address-book"></i><br>
-                  <strong>+91-9434788006</strong></br>
-                  <strong>+91-8465478456</strong></br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card testimonial-card">
-              <div class="card-up">
-              </div>
-              <div class="avatar mx-auto">
-                <img src="/static/img/hodcse.jpg" class="rounded-circle img-responsive">
-              </div>
-              <div class="card-body-in">
-                <h4 class="card-title mt-1"><strong>Dr. Sanyal Gautam</strong></h4>
-                <hr>
-                <p class="min-profile">
-                  <strong>-- Dean (Faculty Welfare) --</strong><br>
-                  <i class="fa fa-envelope"></i><br>
-                  <strong>gautam.sanyal@cse.nitdgp.ac.in<br>nitgsanyal@gmail.com</strong><br>
-                  <i class="fa fa-address-book"></i><br>
-                  <strong>+91-9434788006</strong></br>
-                  <strong>+91-8465478456</strong></br>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+	<links-page>
+		<card title="Web Team">
+			<div class="carousel-inner person-list" role="listbox">
+	      <div class="carousel-item active">
+	        <div class="row">
+						<div class="col" v-for="person in webteam.people">
+							<card-testimonial :name="person.name" :image="person.image" :desig="person.designation">
+								<!-- <strong>-- Research Interest --</strong><br>
+								<span v-html="stripDesc(person.research_interest)"></span><br>
+								<i class="fa fa-envelope"></i><br>
+								<strong>{{ person.email }}</strong><br>
+								<i class="fa fa-address-book"></i><br>
+								<strong>+91-{{ person.mobile }}</strong></br>
+								<span class="grey-text">Joined the Institute in {{ person.joining_year }}
+								</span> -->
+							</card-testimonial>
+						</div>
+	        </div>
+	      </div>
+    	</div>
+		</card>
+  </links-page>
 </template>
 
 <script>
 import axios from 'axios'
+import Card from '@/components/Card'
+import CardTestimonial from '@/components/CardTestimonial'
+import LinksPage from '@/components/LinksPage'
 
 export default {
   name: 'Webteam',
   data () {
+		return {
+			webteam: {
+				people: [
+					{
+						name: "Dr. Saravanan Chandran",
+						image: "",
+						designation: "Faculty in Charge"
+					},
+					{
+						name: "Abhishek Prasad",
+						image: "",
+						designation: "Developer"
+					},
+					{
+						name: "Ankan Poddar",
+						image: "",
+						designation: "Developer"
+					},
+					{
+						name: "Arnav",
+						image: "",
+						designation: "Developer"
+					},
+					{
+						name: "Devansh Goenka",
+						image: "",
+						designation: "Developer"
+					},
+					{
+						name: "Divyanshu Mehta",
+						image: "",
+						designation: "Developer"
+					},
+					{
+						name: "Vivek Modi",
+						image: "",
+						designation: "Developer"
+					},
+					{
+						name: "Parul Chandel",
+						image: "",
+						designation: "Developer"
+					}
+				]
+			}
+		}
   },
   created () {
     this.$emit('hideloader', true)
   },
   components: {
+		Card,
+		CardTestimonial,
+		LinksPage
   }
 }
 </script>
