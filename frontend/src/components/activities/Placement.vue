8ccddd7fc107d37e12229d47f6aa8c12caa1f7e8
Arnav <arnav.kr.grv@gmail.com>
2018-04-17 16:22:27 +0530

    Modified placement page

--- frontend/src/components/activities/Placement.vue
+++ frontend/src/components/activities/Placement.vue
@@ -1,8 +1,23 @@
+<template>
+  	<div class="l1 page-type-links">
+	    <!-- CONTENT -->
+	    <div class="card">
+	      <a class="card-header white-text">Placement Activities</a>
+	      	<div class="card-body">
+				<div class="card-text">
+				  <ul class="list-group list-gr">
+				    <li><a class="list-group-item" target="new" href="#">Placement Statistics</a></li>
+				  </ul>
+				</div>
+	    	</div>
+	    </div>
+   	</div>
+</template>
 <script>
 export default {
   name: 'Placement',
   created () {
-    window.location = 'https://placement.nitdgplug.org/'
+    this.$emit('hideloader', true)
   }
 }
 </script>
