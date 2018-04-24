500fed3c093a761eea31958a8fb5e71b6e56dbaf
Compro-Prasad <comproprasad@gmail.com>
2018-04-24 14:27:56 +0530

    [FEATURE] Added BOG agenda in BOG.vue

--- frontend/src/components/administration/Bog.vue
+++ frontend/src/components/administration/Bog.vue
@@ -1,42 +1,53 @@
 <template>
   <links-page>
-    <card title="Board of Governors">
-      <div class="carousel-inner person-list" role="listbox">
-        <div class="carousel-item active">
-          <div class="row">
-            <div class="col black-text" v-for="person in bogs.chairperson">
-              <card-testimonial :image="person.image" :name="person.name"
-                :desig="person.role">
-                <strong>-- {{ person.designation }} --</strong></br>
-                {{ person.address }}</br>
-              </card-testimonial>
+    <collapse-list>
+      <card-collapse v-if="bogs" title="Board of Governors" :show="true">
+        <div class="carousel-inner person-list" role="listbox">
+          <div class="carousel-item active">
+            <div class="row">
+              <div class="col black-text" v-for="person in bogs.chairperson">
+                <card-testimonial :image="person.image" :name="person.name"
+                  :desig="person.role">
+                  <strong>-- {{ person.designation }} --</strong></br>
+                  {{ person.address }}</br>
+                </card-testimonial>
+              </div>
+              <div class="col black-text" v-for="person in bogs.secratary">
+                <card-testimonial :image="person.image" :name="person.name"
+                  :desig="person.role">
+                  <strong>-- {{ person.designation }} --</strong></br>
+                  {{ person.address }}</br>
+                </card-testimonial>
+              </div>
+              <div class="col black-text" v-for="person in bogs.member">
+                <card-testimonial :image="person.image" :name="person.name"
+                  :desig="person.role">
+                  <strong>-- {{ person.designation }} --</strong></br>
+                  {{ person.address }}</br>
+                </card-testimonial>
+              </div>
             </div>
-            <div class="col black-text" v-for="person in bogs.secratary">
-              <card-testimonial :image="person.image" :name="person.name"
-                :desig="person.role">
-                <strong>-- {{ person.designation }} --</strong></br>
-                {{ person.address }}</br>
-              </card-testimonial>
-            </div>
-            <div class="col black-text" v-for="person in bogs.member">
-              <card-testimonial :image="person.image" :name="person.name"
-                :desig="person.role">
-                <strong>-- {{ person.designation }} --</strong></br>
-                {{ person.address }}</br>
-              </card-testimonial>
-            </div>
-
           </div>
         </div>
-      </div>
-    </card>
+      </card-collapse>
+      <card-collapse v-if="bog_agendas" title="Agenda of BOG Meetings">
+        <ul class="list-group list-gr">
+          <li v-for="bog_agenda in bog_agendas">
+            <a class="list-group-item" target="new" :href="bog_agenda.file">
+              {{ bog_agenda.title }}
+            </a>
+          </li>
+        </ul>
+      </card-collapse>
+    </collapse-list>
   </links-page>
 </template>
 
 <script>
 import axios from 'axios'
 import LinksPage from '@/components/LinksPage'
-import Card from '@/components/Card'
+import CardCollapse from '@/components/CardCollapse'
+import CollapseList from '@/components/CollapseList'
 import CardTestimonial from '@/components/CardTestimonial'
 import { range, genBackendURL } from '@/common.js'
 
@@ -44,14 +55,30 @@ export default {
   name: "Bog",
   data () {
     return {
-      bogs: {}
+      bogs: {},
+      bog_agendas: []
     }
   },
   created () {
+    let count_axios = 0
     axios.get(genBackendURL("administration/bog"))
          .then(response => {
            this.bogs = response.data
-           this.$emit('hideloader', true)
+           if (count_axios == 1) {
+             this.$emit('hideloader', true)
+           }
+           count_axios++
+         })
+         .catch(e => {
+           console.log(e)
+         })
+    axios.get(genBackendURL("administration/bogagenda"))
+         .then(response => {
+           this.bog_agendas = response.data.results
+           if (count_axios == 1) {
+             this.$emit('hideloader', true)
+           }
+           count_axios++
          })
          .catch(e => {
            console.log(e)
@@ -62,7 +89,8 @@ export default {
   },
   components: {
     LinksPage,
-    Card,
+    CardCollapse,
+    CollapseList,
     CardTestimonial
   }
 }
