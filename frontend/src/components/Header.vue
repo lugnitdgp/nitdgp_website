8c067e48a098ba15b79b22148d079750b241ba46
Compro-Prasad <comproprasad@gmail.com>
2018-07-31 19:12:56 +0530

    [UPDATE] Frontend change from server

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,47 +1,34 @@
 <template>
   <header class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-dark darken-2" id="nav-below">
-      <div v-show="header" class="container-fluid" >
-        <div class="navbar-header">
-          <a :class="$route.path == '/' ? 'disabled' : ''" href="/">
-            <img class="navbar-brand" src="/static/img/nitdgp_logo_white.png">
-          </a>
-          <img class="line-img" src="/static/img/line.png">
-          <img class="navbar-img-small" src="/static/img/emblem.png">
-          <div class="navbar-text white-text">
-            <div align="left">
-              <h6>National Institute of Technology Durgapur</h6>
-              <h6> राष्ट्रीय प्रौद्योगिकी संस्थान दुर्गापुर </h6>
-              <p class="navbar-text-small">(An Institute of National Importance under Government of India,<br> Ministry of Human Resource Development)</p>
-            </div>
+    <nav v-show="header" class="navbar navbar-expand-lg container-fluid navbar-dark darken-2" id="nav-below">
+      <div class="navbar-header">
+        <a :class="$route.path == '/' ? 'disabled' : ''" href="/">
+          <img class="navbar-brand" src="/static/img/nitdgp_logo_white.png">
+        </a>
+        <img class="navbar-img-small" src="/static/img/emblem.png">
+        <div class="navbar-text white-text">
+          <div align="left">
+            <h6>National Institute of Technology Durgapur</h6>
+            <h6> राष्ट्रीय प्रौद्योगिकी संस्थान दुर्गापुर </h6>
+            <p v-show="windowWidth > 800" class="navbar-text-small">
+              An Institute of National Importance under Government of India,<br> Ministry of Human Resource Development
+            </p>
           </div>
-          <img class="navbar-img-big"  src="/static/img/emblem.png">
         </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon" style="margin-left: -10px;"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link waves-effect waves-light navbar-link" href="http://nitdgp.ac.in/alumni/index.php"><i class="fa fa-plane"></i>Alumni</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link waves-effect waves-light navbar-link" href="https://mail.google.com/a/nitdgp.ac.in"><i class="fa fa-tasks"></i>Webmail</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link waves-effect waves-light navbar-link" href="/contacts"><i class="fa fa-address-book"></i>Contacts</a>
-            </li>
-          </ul>
-        </div>
+        <img class="navbar-img-big"  src="/static/img/emblem.png">
       </div>
     </nav>
     <nav class="mb-1 navbar fixed-top navbar-expand-sm" id="top-nav-wrap">
       <span class="top-nav-container-left">
-        <a v-if="$route.path != '/'" href="/" class="black-text">
-          <i class="fa fa-home fa-lg fa-2x" aria-hidden="true"></i>
+        <a v-if="$route.path != '/'" href="/" class="top-nav-link">
+          <i class="fa fa-home fa-lg fa-2x" aria-hidden="true"></i> |
         </a>
-        <a @click="toggleHeader()">H</a>
+        <a class="top-nav-link" @click="toggleHeader()">H</a>
+        <div style="display:inline-block">
+        <a class="top-nav-link" href="http://nitdgp.ac.in/alumni/index.php">Alumni</a>
+        <a class="top-nav-link" href="https://mail.google.com/a/nitdgp.ac.in">Webmail</a>
+        <a class="top-nav-link" href="/contacts">Contacts</a>
+        </div>
       </span>
       <span class="top-nav-container-right">
         <input id="search-btn-nav" type="text" name="" placeholder=" Search">
@@ -56,27 +43,33 @@ export default {
   name: 'Header',
   data () {
     return {
-      header: true
+      header: true,
+      windowWidth: 1000
     }
   },
   created () {
     if (window.localStorage["header"] !== undefined)
       this.header = window.localStorage.getItem("header") == "true"
+    window.addEventListener('resize', this.updateWidth)
+    this.windowWidth = document.body.clientWidth
   },
   methods: {
     toggleHeader() {
       this.header = !this.header
       window.localStorage.setItem("header", this.header)
+    },
+    updateWidth () {
+      this.windowWidth = document.body.clientWidth
     }
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.updateWidth);
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #navbar-link {
-    font-size: 90%;
-  }
   .navbar-header{
     margin-top: 1.7em;
   }
@@ -90,25 +83,24 @@ export default {
     padding-left: 5px;
   }
   .top-nav-container-left{
-    width: 22%;
+    width: 60%;
     text-align: left;
   }
   .top-nav-container-right{
-    width: 78%;
+    width: 40%;
     text-align: right;
   }
-  .top-nav-link{
+  .top-nav-link {
     color: #000000;
     padding-left: 10px;
-    font-size: 90%;
+    padding-right: 10px;
+    height: 100%;
+    max-height: 100%;
+    display: inline-block;
+    font-size: 100%;
   }
-  .top-nav-link:not(:nth-child(4))::after {
-    padding: 0 0.2em;
-    content: '>';
-  }
-
-  .top-nav-link:nth-child(4) {
-    padding-right: 0.5em;
+  .top-nav-link:hover {
+    color: #459;
   }
   #nav-below{
     background-color: #001333;
