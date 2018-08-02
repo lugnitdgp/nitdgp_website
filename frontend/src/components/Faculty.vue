1df39f14c3b933e88fb1bbaf6f2418564979e0e2
Compro-Prasad <comproprasad@gmail.com>
2018-08-02 11:14:13 +0530

    [FIX] Empty details on Faculty page

--- frontend/src/components/Faculty.vue
+++ frontend/src/components/Faculty.vue
@@ -35,16 +35,16 @@
           </button>
           <div class="dropdown-menu" id="list-tab" role="tablist">
             <div class="list-group">
-              <a class="dropdown-item active" data-toggle="list" href="#li1" role="tab">Education</a>
-              <a class="dropdown-item" data-toggle="list" href="#li2" role="tab">Work Experience</a>
-              <a class="dropdown-item" data-toggle="list" href="#li3" role="tab">Research Interest</a>
-              <a class="dropdown-item" data-toggle="list" href="#li4" role="tab">Projects</a>
-              <a class="dropdown-item" data-toggle="list" href="#li5" role="tab">Publication</a>
-              <a class="dropdown-item" data-toggle="list" href="#li6" role="tab">Teachings</a>
-              <a class="dropdown-item" data-toggle="list" href="#li7" role="tab">Students</a>
-              <a class="dropdown-item" data-toggle="list" href="#li8" role="tab">Awards and recognitions</a>
-              <a class="dropdown-item" data-toggle="list" href="#li9" role="tab">Administrative Responsibilities</a>
-              <a class="dropdown-item" data-toggle="list" href="#li10" role="tab">Contact</a>
+              <a class="dropdown-item active" :class="!('education' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li1" role="tab">Education</a>
+              <a class="dropdown-item" :class="!('work_experience' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li2" role="tab">Work Experience</a>
+              <a class="dropdown-item" :class="!('research_interest' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li3" role="tab">Research Interest</a>
+              <a class="dropdown-item" :class="!('projects' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li4" role="tab">Projects</a>
+              <a class="dropdown-item" :class="!('publication' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li5" role="tab">Publication</a>
+              <a class="dropdown-item" :class="!('teachings' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li6" role="tab">Teachings</a>
+              <a class="dropdown-item" :class="!('students' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li7" role="tab">Students</a>
+              <a class="dropdown-item" :class="!('awards_and_recognition' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li8" role="tab">Awards and recognitions</a>
+              <a class="dropdown-item" :class="!('administrative_responsiblities' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li9" role="tab">Administrative Responsibilities</a>
+              <a class="dropdown-item" :class="!('contact' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li10" role="tab">Contact</a>
             </div>
           </div>
         </div>
@@ -54,25 +54,29 @@
           <div class="tab-pane fade show active big-list" id="li1" role="tabpanel">
             <h3 class="pane-title" align="left">Education</h3>
             <hr>
-            <span v-html="faculty.education" class="container" />
+            <span v-if="'education' in faculty" v-html="faculty.education"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
 
           <div class="tab-pane fade big-list" id="li2" role="tabpanel">
             <h3 class="pane-title" align="left">Work Experiences</h3>
             <hr>
-            <span v-html="faculty.work_experience" class="container" />
+            <span v-if="'work_experience' in faculty" v-html="faculty.work_experience"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
 
           <div class="tab-pane fade big-list" id="li3" role="tabpanel">
             <h3 class="pane-title" align="left">Research Interest</h3>
             <hr>
-            <span v-html="faculty.research_interest" class="container" />
+            <span v-if="'research_interest' in faculty" v-html="faculty.research_interest"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
 
           <div class="tab-pane fade big-list" id="li4" role="tabpanel">
             <h3 class="pane-title" align="left">Projects</h3>
             <hr>
-            <span v-html="faculty.projects" class="container" />
+            <span v-if="'projects' in faculty" v-html="faculty.projects"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
 
           <div class="tab-pane fade big-list" id="li5" role="tabpanel">
@@ -106,12 +110,9 @@
             <h3 class="pane-title" align="left">Teachings</h3>
             <hr>
             <ul class="nav md-pills nav-justified pills-secondary">
-              <li class="nav-item">
-                <a class="nav-link active" data-toggle="tab" href="#p6l1" role="tab">B-Tech &amp; M-Tech</a>
+            <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#p6l1" role="tab">B-Tech &amp; M-Tech</a>
               </li>
-              <!-- <li class="nav-item">
-                   <a class="nav-link active" data-toggle="tab" href="#p6l2" role="tab">ccc</a>
-                   </li> -->
             </ul>
             <div class="tab-content">
               <div class="tab-pane fade in show active" id="p6l1" role="tabpanel">
@@ -146,34 +147,31 @@
                   </div>
                 </div>
               </div>
-              <!-- <div class="tab-pane fade" id="p6l2" role="tabpanel">
-                   xxx
-                   </div> -->
             </div>
           </div>
           <div class="tab-pane fade big-list" id="li7" role="tabpanel">
             <h3 class="pane-title" align="left">List of students</h3>
             <hr>
-            <h4 class="red-text" v-if="!faculty.students">Not Available</h4>
-            <span v-html="faculty.students"/>
+            <span v-if="'students' in faculty" v-html="faculty.students"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
           <div class="tab-pane fade big-list" id="li8" role="tabpanel">
             <h3 class="pane-title" align="left">Awards and Recognitions</h3>
             <hr>
-            <h4 class="red-text" v-if="!faculty.awards_and_recognition">Not Available</h4>
-            <span v-html="faculty.awards_and_recognition"/>
+            <span v-if="'awards_and_recognition' in faculty" v-html="faculty.awards_and_recognition"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
           <div class="tab-pane fade big-list" id="li9" role="tabpanel">
             <h3 class="pane-title" align="left">Administrative Responsibilities</h3>
             <hr>
-            <h4 class="red-text" v-if="!faculty.administrative_responsibilities">Not Available</h4>
-            <span v-html="faculty.administrative_responsibilities"/>
+            <span v-if="'administrative_responsibilities' in faculty" v-html="faculty.administrative_responsibilities"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
           <div class="tab-pane fade big-list" id="li10" role="tabpanel">
             <h3 class="pane-title" align="left">Contact</h3>
             <hr>
-            <h4 class="red-text" v-if="!faculty.contact">Not Available</h4>
-            <span v-html="faculty.contact"></span>
+            <span v-if="'contact' in faculty" v-html="faculty.contact"/>
+            <h4 v-else class="red-text">Not Available</h4>
           </div>
         </div>
       </div>
@@ -211,6 +209,12 @@ export default {
     axios.get(genBackendURL("faculty/" + this.$route.params.id))
          .then(response => {
            this.faculty = response.data
+           for (var key in this.faculty) {
+             if (this.faculty.hasOwnProperty(key) &&
+                 Object.keys(this.faculty[key]).length === 0) {
+               delete this.faculty[key];
+             }
+           }
            console.log(this.faculty)
            this.$emit('hideloader', true)
          })
