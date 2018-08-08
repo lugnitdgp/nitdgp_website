3b2df184398b6cb89c18c05753c4f65e80fe56d9
Compro-Prasad <comproprasad@gmail.com>
2018-08-08 15:22:31 +0530

    Corrected positioning of popup menu and bits of fixes

--- frontend/src/components/Faculty.vue
+++ frontend/src/components/Faculty.vue
@@ -14,7 +14,7 @@
       <div class="container-fluid card fac-sm">
         <div class="row">
           <div class="photo" :class="getClassImg()">
-            <img :src="faculty.image" style="border: solid grey 2px;max-width: 100%; max-height: 100%">
+            <img :src="faculty.image" style="max-width: 100%; max-height: 100%">
             <p style="font-weight: bold">{{ faculty.name }}</p>
             <p>{{ faculty.designation }}</p>
             <i class="fa fa-mortar-board"></i><br>
@@ -26,7 +26,7 @@
               {{ faculty.email }}
             </p>
           </div>
-          <div class="downc card tab-content" :class="getClassContent()">
+          <div class="downc tab-content" :class="getClassContent()">
             <button class="btn" type="button" @click="showNav($event)">
               <i class="fa fa-bars fa-2x"></i>
             </button>
@@ -153,17 +153,19 @@
               <h4 v-else class="red-text">Not Available</h4>
             </div>
           </div>
-          <div class="list-group" :class="extraNavClasses.concat([getClassNav()])">
-            <a class="dropdown-item active" :class="!('education' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li1" role="tab">Education</a>
-            <a class="dropdown-item" :class="!('work_experience' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li2" role="tab">Work Experience</a>
-            <a class="dropdown-item" :class="!('research_interest' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li3" role="tab">Research Interest</a>
-            <a class="dropdown-item" :class="!('projects' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li4" role="tab">Projects</a>
-            <a class="dropdown-item" :class="!('publication' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li5" role="tab">Publication</a>
-            <a class="dropdown-item" :class="!('teachings' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li6" role="tab">Teachings</a>
-            <a class="dropdown-item" :class="!('students' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li7" role="tab">Students</a>
-            <a class="dropdown-item" :class="!('awards_and_recognition' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li8" role="tab">Awards and recognitions</a>
-            <a class="dropdown-item" :class="('administrative_responsiblities' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li9" role="tab">Administrative Responsibilities</a>
-            <a class="dropdown-item" :class="!('contact' in faculty) ? 'disabled' : ''" data-toggle="list" href="#li10" role="tab">Contact</a>
+          <div class="list-group"
+            :class="extraNavClasses.concat([getClassNav()])"
+            :style="{ top: popUpTop + 'px' }">
+            <a class="dropdown-item active" :class="{ 'disabled': !('education' in faculty) }" data-toggle="list" href="#li1" role="tab">Education</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('work_experience' in faculty) }" data-toggle="list" href="#li2" role="tab">Work Experience</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('research_interest' in faculty) }" data-toggle="list" href="#li3" role="tab">Research Interest</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('projects' in faculty) }" data-toggle="list" href="#li4" role="tab">Projects</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('publication' in faculty) }" data-toggle="list" href="#li5" role="tab">Publication</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('teachings' in faculty) }" data-toggle="list" href="#li6" role="tab">Teachings</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('students' in faculty) }" data-toggle="list" href="#li7" role="tab">Students</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('awards_and_recognition' in faculty) }" data-toggle="list" href="#li8" role="tab">Awards and recognitions</a>
+            <a class="dropdown-item" :class="{ 'disabled': ('administrative_responsiblities' in faculty) }" data-toggle="list" href="#li9" role="tab">Administrative Responsibilities</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('contact' in faculty) }" data-toggle="list" href="#li10" role="tab">Contact</a>
           </div>
           <div v-show="nav" id="fade" class="black_overlay"></div>
         </div>
@@ -198,7 +200,9 @@ export default {
       general: true,
       windowWidth: 1000,
       extraNavClasses: [],
-      nav: false
+      nav: false,
+      popUpTop: 0,
+      top: -1
     }
   },
   created () {
@@ -211,7 +215,6 @@ export default {
                delete this.faculty[key];
              }
            }
-           console.log(this.faculty)
            this.$emit('hideloader', true)
          })
          .catch(e => {
@@ -223,7 +226,6 @@ export default {
   },
   mounted () {
     Array.from(document.getElementsByClassName('dropdown-item')).forEach(el => {
-      console.log("Hiding Div", el)
       el.addEventListener('click', this.hideNav)
     })
   },
@@ -231,19 +233,19 @@ export default {
     hideNav () {
       this.nav = false
       this.extraNavClasses = []
+      this.popUpTop = 0
     },
     showNav (event) {
-      console.log(event)
       this.extraNavClasses = ['lift', 'div-block']
       this.nav = true
-      document.getElementsByClassName('lift')[0].style.top = event.clientY.toString() + 'px'
+      this.popUpTop = event.clientY + this.top * 200
     },
     getClassImg () {
       let width = this.windowWidth
       if (width > 1180)
         return 'col-2'
       else if (width > 900)
-        return 'col-3'
+        return 'col-2'
       else if (width > 600)
         return 'col-4'
       else
@@ -254,11 +256,15 @@ export default {
       if (width > 1180)
         return 'col-8'
       else if (width > 900)
-        return 'col-6'
-      else if (width > 600)
+        return 'col-7'
+      else if (width > 600) {
+        this.top = -1
         return 'col-8'
-      else
+      }
+      else {
+        this.top = 1
         return 'col-12'
+      }
     },
     getClassNav () {
       let width = this.windowWidth
