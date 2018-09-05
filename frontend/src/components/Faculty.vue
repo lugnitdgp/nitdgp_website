9373f379e32185ab94107f5bb0ea0bc6e15b2628
Compro-Prasad <comproprasad@gmail.com>
2018-09-05 07:18:54 +0530

    [FIX Faculty.vue] this.faculty[0] replaced with this.faculty
    this.faculty[0] wasn't being utilized everywhere which was causing
    issues. Using this.faculty as an array isn't the best way because it
    will only have a single element.

--- frontend/src/components/Faculty.vue
+++ frontend/src/components/Faculty.vue
@@ -3,8 +3,8 @@
     <sp-card containerclass="l3-card">
       <div slot="header">
         <p class="tile-title-text">
-          <a class="white-text" :href="'/department/' + faculty[0].dept_short_code">
-            FACULTY | {{ faculty[0].dept_short_code }}
+          <a class="white-text" :href="'/department/' + faculty.dept_short_code">
+            FACULTY | {{ faculty.dept_short_code }}
           </a>
           <span v-if="!general" @click="general = true">
             - Show
@@ -14,14 +14,14 @@
       <div class="container-fluid fac-sm">
         <div class="row">
           <div class="photo" :class="getClassImg()">
-            <img :src="faculty[0].image" style="max-width: 100%; max-height: 100%">
-            <p style="font-weight: bold">{{ faculty[0].name }}</p>
-            <p>{{ faculty[0].designation }}</p>
+            <img :src="faculty.image" style="max-width: 100%; max-height: 100%">
+            <p style="font-weight: bold">{{ faculty.name }}</p>
+            <p>{{ faculty.designation }}</p>
             <p style="font-size: 80%">
-              <strong v-if="faculty[0].joining_year==1959" >Joined in N/A<br></strong>
-              <strong v-if="faculty[0].joining_year!=1959" >Joined in {{ faculty[0].joining_year }}<br></strong>
+              <strong v-if="faculty.joining_year==1959" >Joined in N/A<br></strong>
+              <strong v-if="faculty.joining_year!=1959" >Joined in {{ faculty.joining_year }}<br></strong>
               <i class="fa fa-envelope"></i><br>
-              {{ faculty[0].email }}
+              {{ faculty.email }}
             </p>
           </div>
           <div class="downc tab-content" :class="getClassContent()">
@@ -31,28 +31,28 @@
             <div class="tab-pane fade show active big-list" id="li1" role="tabpanel">
               <h3 class="pane-title" align="left">Education</h3>
               <hr>
-              <span v-if="'education' in faculty[0]" v-html="faculty[0].education"/>
+              <span v-if="'education' in faculty" v-html="faculty.education"/>
               <h4 v-else class="red-text">Not Available</h4>
             </div>
 
             <div class="tab-pane fade big-list" id="li2" role="tabpanel">
               <h3 class="pane-title" align="left">Work Experiences</h3>
               <hr>
-              <span v-if="'work_experience' in faculty[0]" v-html="faculty[0].work_experience"/>
+              <span v-if="'work_experience' in faculty" v-html="faculty.work_experience"/>
               <h4 v-else class="red-text">Not Available</h4>
             </div>
 
             <div class="tab-pane fade big-list" id="li3" role="tabpanel">
               <h3 class="pane-title" align="left">Research Interest</h3>
               <hr>
-              <span v-if="'research_interest' in faculty[0]" v-html="faculty[0].research_interest"/>
+              <span v-if="'research_interest' in faculty" v-html="faculty.research_interest"/>
               <h4 v-else class="red-text">Not Available</h4>
             </div>
 
             <div class="tab-pane fade big-list" id="li4" role="tabpanel">
               <h3 class="pane-title" align="left">Projects</h3>
               <hr>
-              <span v-if="'projects' in faculty[0]" v-html="faculty[0].projects"/>
+              <span v-if="'projects' in faculty" v-html="faculty.projects"/>
               <h4 v-else class="red-text">Not Available</h4>
             </div>
 
@@ -76,18 +76,18 @@
               <div class="tab-content">
                 <div class="tab-pane fade in show active" id="p5l1" role="tabpanel">
                   <h3>Journals</h3>
-                  <table-renderer :table="faculty[0].journals" :theader="['Citation','Journal','Year']"></table-renderer>
+                  <table-renderer :table="faculty.journals" :theader="['Citation','Journal','Year']"></table-renderer>
                 </div>
                 <div class="tab-pane fade page-type-links" id="p5l2" role="tabpanel">
                   <ul class="list-group list-gr">
                   <h3>Conferences</h3>
-                  <table-renderer :table="faculty[0].conferences" :theader="['Citation','Location','Year']"></table-renderer>
+                  <table-renderer :table="faculty.conferences" :theader="['Citation','Location','Year']"></table-renderer>
                   </ul>
                 </div>
                 <div class="tab-pane fade in" id="p5l3" role="tabpanel">
                   <h3>Books</h3>
                   <ul>
-                    <li v-for="book in faculty[0].books" >
+                    <li v-for="book in faculty.books" >
                       <a v-if="book.file" :href="book.file" target="new">{{ book.title }}</a>
                       <a v-else :href="book.url" target="new">{{ book.title }}</a>
                     </li>
@@ -96,7 +96,7 @@
                 <div class="tab-pane fade in" id="p5l4" role="tabpanel">
                   <h3>Patents</h3>
                     <ul>
-                      <li v-for="patent in faculty[0].patents" >
+                      <li v-for="patent in faculty.patents" >
                         <a v-if="patent.file" :href="patent.file" target="new">{{ patent.title }}</a>
                       </li>
                     </ul>
@@ -107,17 +107,17 @@
             <div class="tab-pane fade big-list" id="li6" role="tabpanel">
               <h3 class="pane-title" align="left">Teachings</h3>
               <hr>
-              <span v-if="'teachings' in faculty[0]" v-html="faculty[0].teachings"/>
+              <span v-if="'teachings' in faculty" v-html="faculty.teachings"/>
               <h4 v-else class="red-text">Not Available</h4>
             </div>
             <div class="tab-pane fade big-list" id="li7" role="tabpanel">
               <h3 class="pane-title" align="left">List of students</h3>
               <hr>
-              <div v-if="'students' in faculty[0]">
+              <div v-if="'students' in faculty">
                 <div class="carousel-inner person-list" role="listbox">
                   <div class="carousel-item active">
                     <div class="row">
-                      <div class="col staffs" v-for="student in faculty[0].students">
+                      <div class="col staffs" v-for="student in faculty.students">
                         <card-testimonial
                           :name="student.name"
                           :image="student.image"
@@ -134,35 +134,35 @@
             <div class="tab-pane fade big-list" id="li8" role="tabpanel">
               <h3 class="pane-title" align="left">Awards and Recognitions</h3>
               <hr>
-              <span v-if="'awards_and_recognition' in faculty[0]" v-html="faculty[0].awards_and_recognition"/>
+              <span v-if="'awards_and_recognition' in faculty" v-html="faculty.awards_and_recognition"/>
               <h4 v-else class="red-text">Not Available</h4>
             </div>
             <div class="tab-pane fade big-list" id="li9" role="tabpanel">
               <h3 class="pane-title" align="left">Administrative Responsibilities</h3>
               <hr>
-              <span v-if="'administrative_responsibilities' in faculty[0]" v-html="faculty[0].administrative_responsibilities"/>
+              <span v-if="'administrative_responsibilities' in faculty" v-html="faculty.administrative_responsibilities"/>
               <h4 v-else class="red-text">Not Available</h4>
             </div>
             <div class="tab-pane fade big-list" id="li10" role="tabpanel">
               <h3 class="pane-title" align="left">Contact</h3>
               <hr>
-              <span>Mobile : +91-{{faculty[0].mobile}}<br/>
-                    Email : {{faculty[0].email}}<br/>
+              <span>Mobile : +91-{{faculty.mobile}}<br/>
+                    Email : {{faculty.email}}<br/>
               </span>
             </div>
           </div>
           <div class="list-group"
             :class="extraNavClasses.concat([getClassNav()])"
             :style="{ top: popUpTop + 'px' }">
-            <a class="dropdown-item active" :class="{ 'disabled': !('education' in faculty[0]) }" data-toggle="list" href="#li1" role="tab">Education</a>
-            <a class="dropdown-item" :class="{ 'disabled': !('work_experience' in faculty[0]) }" data-toggle="list" href="#li2" role="tab">Work Experience</a>
-            <a class="dropdown-item" :class="{ 'disabled': !('research_interest' in faculty[0]) }" data-toggle="list" href="#li3" role="tab">Research Interest</a>
-            <a class="dropdown-item" :class="{ 'disabled': !('projects' in faculty[0]) }" data-toggle="list" href="#li4" role="tab">Projects</a>
+            <a class="dropdown-item active" :class="{ 'disabled': !('education' in faculty) }" data-toggle="list" href="#li1" role="tab">Education</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('work_experience' in faculty) }" data-toggle="list" href="#li2" role="tab">Work Experience</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('research_interest' in faculty) }" data-toggle="list" href="#li3" role="tab">Research Interest</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('projects' in faculty) }" data-toggle="list" href="#li4" role="tab">Projects</a>
             <a class="dropdown-item" data-toggle="list" href="#li5" role="tab">Publication</a>
-            <a class="dropdown-item" :class="{ 'disabled': !('teachings' in faculty[0]) }" data-toggle="list" href="#li6" role="tab">Teachings</a>
-            <a class="dropdown-item" :class="{ 'disabled': !('students' in faculty[0]) }" data-toggle="list" href="#li7" role="tab">Students</a>
-            <a class="dropdown-item" :class="{ 'disabled': !('awards_and_recognition' in faculty[0]) }" data-toggle="list" href="#li8" role="tab">Awards and recognitions</a>
-            <a class="dropdown-item" :class="{ 'disabled': !('administrative_responsibilities' in faculty[0]) }" data-toggle="list" href="#li9" role="tab">Administrative Responsibilities</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('teachings' in faculty) }" data-toggle="list" href="#li6" role="tab">Teachings</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('students' in faculty) }" data-toggle="list" href="#li7" role="tab">Students</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('awards_and_recognition' in faculty) }" data-toggle="list" href="#li8" role="tab">Awards and recognitions</a>
+            <a class="dropdown-item" :class="{ 'disabled': !('administrative_responsibilities' in faculty) }" data-toggle="list" href="#li9" role="tab">Administrative Responsibilities</a>
             <a class="dropdown-item" data-toggle="list" href="#li10" role="tab">Contact</a>
             <a class="dropdown-item" :href="genBackendURL('admin')" target="new" role="tab">Faculty Login</a>
           </div>
@@ -196,8 +196,8 @@ export default {
   created () {
     axios.get(genBackendURL("faculty/" + this.$route.params.id))
          .then(response => {
-           this.faculty = response.data.faculty
-           console.log(this.faculty[0].mobile)
+           this.faculty = response.data.faculty[0]
+           console.log(this.faculty.mobile)
            for (var key in this.faculty) {
              if (this.faculty.hasOwnProperty(key) &&
                  Object.keys(this.faculty[key]).length === 0) {
