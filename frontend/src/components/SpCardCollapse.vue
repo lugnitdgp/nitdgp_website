a297e3fd743bc45b95c678d4ad232297a3efac41
Compro-Prasad <comproprasad@gmail.com>
2018-09-05 12:36:47 +0530

    [FIX Home] Added back the animation. A section hides at a time.

--- frontend/src/components/SpCardCollapse.vue
+++ frontend/src/components/SpCardCollapse.vue
@@ -4,7 +4,7 @@
       class="view gradient-card-header tile-title">
       <slot name="header"></slot>
     </a>
-    <div class="card-body collapse" :class="bodyclass + (hide == false ? ' ' : ' show')" :id="bodyId">
+    <div class="card-body collapse" :class="bodyclass + (hide == true ? ' hide_me' : ' show_me')" :id="bodyId">
       <slot></slot>
     </div>
   </div>
@@ -19,7 +19,8 @@ export default {
     expansion() {
       console.log(this.row_index)
       this.hide = !this.hide
-      this.$root.$emit('test', this.row_index, this.hide)
+      if (this.hide == false)
+        this.$root.$emit('show-all', this.row_index)
     }
   },
   props: {
@@ -49,14 +50,32 @@ export default {
   },
   created () {
     this.bodyId = getUniqueId("spc-card-", 4)
-    this.hide = this.show
-    this.$root.$on('test', (index, state) => {
+    this.hide = !this.show
+    this.$root.$on('show-all', index => {
       if (index == this.row_index)
-        this.hide = state
+        this.hide = false
     })
   }
 }
 </script>
 
 <style scoped>
+  div {
+    transition: all .4s ease !important;
+  }
+  .hide_me {
+    display: none;
+    max-height: 0;
+  }
+  .show_me {
+    display: block;
+    animation-duration: .6s;
+    animation-name: fadeIn;
+    animation-delay: 0s;
+    animation-timing-function: initial;
+    animation-iteration-count: initial;
+    animation-direction: initial;
+    animation-fill-mode: initial;
+    animation-play-state: initial;
+  }
 </style>
