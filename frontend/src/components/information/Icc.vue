f3607bedd5d22a13ed4bfd9d5f3100dd151abe72
Divyanshu Mehta <divyanshumehta@outlook.com>
2018-10-03 15:38:10 +0530

    ICC static info added

--- frontend/src/components/information/Icc.vue
+++ frontend/src/components/information/Icc.vue
@@ -1,5 +1,18 @@
 <template>
   <links-page>
+    <card title="ICC Members">
+      <div class="carousel-inner person-list" role="listbox">
+				<div class="carousel-item active">
+					<div class="row">
+						<div class="col" v-for="person in members">
+							<card-testimonial :name="person.name" :image="person.image" :desig="person.title">
+							{{person.designation}}
+							</card-testimonial>
+						</div>
+					</div>
+				</div>
+      </div>
+    </card>
     <card title="ICC">
       <ul class="list-group list-gr">
         <li v-for="icc in iccs">
@@ -17,6 +30,7 @@
 
 <script>
 import Card from "@/components/Card"
+import CardTestimonial from '@/components/CardTestimonial'
 import LinksPage from "@/components/LinksPage"
 import axios from 'axios'
 import { genBackendURL } from '@/common.js'
@@ -25,7 +39,16 @@ export default {
   name: "Icc",
   data () {
     return {
-      iccs: []
+      iccs: [],
+      members: [
+        {name:"Prof.(Mrs.) Kajla Basu", title:"Prof. of Mathematics", designation:"Presiding Officer", image:"https://admin.nitdgp.ac.in/files/faculty/Kajla%20Basu/kajla_basu.jpg"},
+        {name:"Mrs. Soma Sinha", title:"Advocate, Calcutta High Court", designation:"Member(External)" },
+        {name:"Prof A. Gangopadhyay", title:"Prof, Dept of EES" ,designation:"Member", image:"https://admin.nitdgp.ac.in/files/faculty/Aniruddha%20Gangopadhyay/aniruddha_ganguly.jpg"},
+        {name:"Dr (Ms.) Mamata Dalui", title:"Astt. Prof. C.Sc. Deptt" ,designation:"Member", image:"https://admin.nitdgp.ac.in/files/faculty/Mamata%20Dalui%20(Chakraborty)/m_dalui.jpg"},
+        {name:"Dr. Kazy Sufia Khannam", title:"Astt. Prof Deptt. of BT" ,designation:"Member", image:"https://admin.nitdgp.ac.in/files/faculty/Sufia%20Khannam%20Kazy/Sufia_Kazy.jpg"},
+        {name:"Dr. G. Prabhavathi", title:"Medical Officer, NITD" ,designation:"Member" },
+        {name:"Dr. (Mrs.) Debjani Dutta", title:"Astt. Prof. Deptt. of BT" ,designation:"Member & Convenor", image:"https://admin.nitdgp.ac.in/files/faculty/Debjani%20Dutta/debjani_dutta.jpg"}
+      ]
     }
   },
   created () {
@@ -40,6 +63,7 @@ export default {
   },
   components: {
     Card,
+    CardTestimonial,
     LinksPage
   }
 }
