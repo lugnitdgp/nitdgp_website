5a6456e9931160f49f81b25eb2a014a94a137deb
Divyanshu Mehta <divyanshumehta@outlook.com>
2018-10-03 14:52:13 +0530

    Static RTI page added

--- frontend/src/components/information/Rti.vue
+++ frontend/src/components/information/Rti.vue
@@ -1,6 +1,226 @@
 <template>
   <links-page>
     <card title="Right to Information">
+      
+      <!-- STATIC CONTENT START -->
+      <a class="link" href="http://old.nitdgp.ac.in/RTI/rti-act.pdf"></a><h6><a class="link" href="http://old.nitdgp.ac.in/RTI/rti-act.pdf"><b>The RTI Act 2005 and the Related Information</b></a></h6>
+      <a class="link" href="http://old.nitdgp.ac.in/RTI/application_form.pdf"></a><h6><a class="link" href="http://old.nitdgp.ac.in/RTI/application_form.pdf"><b>RTI Application Form</b></a></h6>
+      
+      
+    <h3 align="center">RTI SECTION 4</h3>
+    <table class="table table-bordered">
+    <tbody>
+      <tr>
+        <td colspan="2"><h4>1. About Act</h4></td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">1.1 Name &amp; Title of the Act</td>
+        <td style="width: 80%;"><a class="link" href="http://old.nitdgp.ac.in/RTI/1.1 RTI ACT 2005.pdf"></a><h6><a class="link" href="http://old.nitdgp.ac.in/RTI/1.1 RTI ACT 2005.pdf"><b>Right to Information Act.2005</b></a></h6>
+            <a class="link" href="http://old.nitdgp.ac.in/RTI/1.1 RTI ACT 2012.pdf"><h6><b>Right to Information Act.2012</b></h6></a></td>
+       
+    </tr>
+    <tr>
+        <td style="width: 20%;">1.2 Definition</td>
+        <td style="width: 80%;">
+Right to Information means the right to inspection of work, documents, records taking notes, extracts or, certified copies of documents or records; taking certified samples of material; obtaining information in the form of diskettes, floppies, tapes, video cassettes or in any other electronic mode or through printouts where such information is stored in a computer or in any other device subject to relevant provisions in this regard.
+</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">1.3 Objective/purpose of the Act</td>
+        <td style="width: 80%;">To provide available information of the Institute as enshrined in RTI ACT to the Indian citizen on payment of prescribed fees.</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">1.4 Users</td>
+        <td style="width: 80%;">Citizen of India.</td>
+    </tr>
+</tbody></table><br/><br/>
+<table class="table table-bordered">
+    <tbody><tr>
+        <td colspan="2"><h4>2. Name and Address of Central Public Information Officer and Appellate Authority</h4></td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">Central Public Information Officer (CPIO)</td>
+        <td style="width: 80%;">Mr. Debashish Mondal<br> Assistant Registrar<br> National Institute of Technology Durgapur<br> Mahatma Gandhi Avenue Durgapur-713209,<br> West Bengal, India <br>Phone Number 0343-2752008 <br>Email: debasish.mondal@admin.nitdgp.ac.in </td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">First Appellate Authority</td>
+        <td style="width: 80%;">Registrar <br>National Institute of Technology Durgapur<br> Mahatma Gandhi Avenue Durgapur-713213,<br> West Bengal, India <br>Emial: registrar@admin.nitdgp.ac.in</td>
+    </tr>
+</tbody></table><br/><br/>
+      <table class="table table-bordered">
+    <tbody><tr><td colspan="2"><h3>3. Act and statues of the Institute</h3></td></tr>
+    <tr>
+        <td style="width: 80%;">3.1 THE NATIONAL INSTITUTES OF TECHNOLOGY PDF FORM | ACT, 2007(N.T.T’S ACT 2007)</td>
+        <td style="width: 20%;"><a href="http://old.nitdgp.ac.in/RTI/3.1 THE NATIONAL INSTITUTES OF TECHNOLOGY ACT, 2007(N.T.T’S ACT 2007).pdf">PDF</a></td>
+    </tr>
+    <tr>
+        <td style="width: 80%;">3.2 the National Institutes of Technology Pdf Form (Amendment) Act, 2012 </td>
+        <td style="width: 20%;"><a href="http://old.nitdgp.ac.in/RTI/3.2 THE NATIONAL INSTITUTES OF TECHNOLOGY (AMENDMENT) ACT, 2012.pdf">PDF</a></td>
+    </tr>
+    <tr>
+        <td style="width: 80%;">3.3 First Statutes of N.I.Ts</td>
+        <td style="width: 20%;"><a href="http://old.nitdgp.ac.in/RTI/3.3 FIRST STATUTES OF N.I.TS.pdf">PDF</a></td>
+    </tr>
+    <tr>
+        <td style="width: 80%;">3.4 First Statutes of National Institute Pdf Form of Technology (Amendent) Statutes, 2017</td>
+        <td style="width: 20%;"><a href="http://old.nitdgp.ac.in/RTI/3.4 FIRST STATUTES OF NATIONAL INSTITUTE OF TECHNOLOGY (AMENDENT) STATUTES, 2017.pdf">PDF</a></td>
+    </tr>
+    </tbody></table><br/><br/>
+    <table class="table table-bordered">
+    <tbody><tr><td colspan="2"><h3>4. About Organisation</h3></td></tr>
+    <tr>
+        <td style="width: 20%;">4.1 Name</td>
+        <td style="width: 80%;">National Institute of Technology Durgapur Mahatma Gandhi Avenue, West Bengal, Pin-713209 India</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">4.2 Brief history of the Institute</td>
+        <td style="width: 80%;text-align:justify;">The National Institute of Technology, Durgapur (formerly Regional Engineering College, Durgapur), was established by an Act of Parliament in 1960 as one of the eight such colleges aimed to function as a pace setter for engineering education in the country and to foster national integration. It is a fully-funded premier Technological Institution of the Government of India and is administered by an autonomous Board of Governors. The Institute is a University which awards B.Tech., M.C.A., M.Sc., M.B.A., M.Tech. and Ph.D. degrees to students after their successful completion of the specified courses. The Institute imparts education in the disciplines of Chemical Engineering, Civil Engineering, Computer Science and Engineering, Electrical Engineering, Electronics and Communication Engineering, Mechanical Engineering, Metallurgical and Materials Engineering, Information Technology, Biotechnology, Physics, Chemistry, Mathematics, Environmental science, Materials Science and Management Studies. As decided by the Ministry of Human Resource Development, Government of India, the procedure for selection of candidates for admission to the Bachelor Degree Courses in Engineering Technology in National Institute of Technology Durgapur and in other NITs is on the basis of State Rank/ All India Rank (AIR) of AIEEE conducted by Central Board of Secondary Education, New Delhi, and the same is executed through counselling by Central Counselling Board, AIEEE under guidance from MHRD, GOI as per schedule notified by CCB. In addition to the normal intake, a few seats are reserved for Foreign Students who are nominated by the Ministry of External Affairs, Government of India, and the Indian Council for Cultural Relations, Government of India.</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">4.3 Location</td>
+        <td style="width: 80%;text-align:justify;">The Institute is located about 160 KMs north-west of Kolkata on the Howrah-Delhi main railway route and overlooking the National Highway No. 2(the great Grand- Trunk Road). The Institute spreads over an area of 187 acres of land. It is fully residential and co-educational. At present about 2500 boys and 500 girls reside in seven boys' hostels and two girls' hostels. The annual undergraduate intake is more than 1000 students.</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">4.4 Administration</td>
+        <td style="width: 80%;text-align:justify;">The highest body in the administrative setup of the Institute is the Board of Governors which is headed by the eminent educationist Prof. Anupam Basu Chairperson (Acting), the Director National Institute of Technology, Durgapur &amp; Professor (on-lien) Dept. of Computer Science &amp; Engineering (CSE) IIT Kharagpur 721302. The Board has representatives from the State and Central Governments, the University, All India Council of Technical Education, Indian Institute of Technology, the Industry and the Faculty of the Institute. The Director is the Head of Administration in the Institute.</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">4.5 Organizational Structure</td>
+        <td style="width: 80%;text-align:justify;">The Director is the principal academic and executive officer of the Institute. He reports to the Board of Governors. The Chairman of the Board executes the contract between the Director and the Institute. The Director is Chairman of the Senate. The Senate comprising all the professors and HODs of the Institute and external members decide the academic policy of the Institute. The Director is assisted in the management of the affairs of the Institute by six Deans: Dean - Academic, Dean - Student Welfare, Dean - Research &amp; Consultancy, Dean- Faculty Welfare , Dean- Planning &amp; Development, Dean- Alumni Affairs &amp; Outreach.
+</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">4.6 Vision, Mission and key Objection</td>
+        <td style="width: 80%;"><a href="http://old.nitdgp.ac.in/RTI/4.6 Vision , Mission and key    Objection.pdf">PDF</a></td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">4.7 Functions and Duties</td>
+        <td style="width: 80%;">As per Act and Statutes of the Institute</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;">4.8 Powers and duties of its officers and employees</td>
+        <td style="width: 80%;text-align:justify;">As per Clause 17 of National Institute of Technology Act, 2007 Director is the Principal Academic and executive officer of the Institute and is responsible for administration of the Institute and for imparting instructions and maintenance of discipline therein. Subject to the budget provisions made for the specific purpose the Director has the power to incur expenditure in accordance with the procedure and guidelines laid down by Board of Governors and MHRD, GOI.
+As per Clause 18 of National Institute of Technology Act, 2007 The Registrar of the Institute is the custodial of records, the common seal, the funds of the Institute and such other property of the Institute. He is responsible to the Director for the proper discharge of his functions.
+Other officers and staff members of the Institute are responsible for the functions and duties assigned to them by the Director from time to time.
+</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.9 The procedure
+followed in the decision making Process, including channels of supervision and accountability.
+</td>
+        <td style="width: 80%;text-align:justify;">As per the NIT Act, 2007, different committees of the Institute such as Finance Committee and Building and Works Committee are empowered in decision making in their respective fields. The Board of Governors is the final authority in supervising all the activities and decisions taken at different levels.
+The Institute is administered by the Director and assisted by six Deans, Registrar, Deputy Registrars and Assistant Registrar who carry out different functions and responsibilities as laid down in the NIT Act, 2007 and NIT Statutes, 2009 and by the Board of Governors from time to time.
+Senate is the highest Academic Authority of the Institute and is empowered to take all decisions regarding academic matters. The recommendations of the Senate are finally considered by the Board of Governors.
+</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.10 The norms set by it for the discharge of its functions.</td>
+        <td style="width: 80%;text-align:justify;">Norms set by the MHRD, Govt. of India and the Board of Governors are used for discharge of administrative functions of the Institute. UGAC, PGAC and BOS (Research) recommend norms for implementation in UG, PG and Research programmes respectively. However all the norms and standards for all academic programmes are finally considered by the Senate.</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.11 The rules, regulations,
+instructions, manuals and records, held by it or under its control or used by its employees for discharging its functions.</td>
+        <td style="width: 80%;text-align:justify;">
+            <ol type="i">
+                <li>As per Act and Statutes of the Institute</li>
+                <li>Fundamental Rules </li>
+                <li>Supplementary Rules</li>
+                <li>General Finance Rules</li>
+                <li>Ordinances &amp; Regulation for all courses.</li>
+                <li>Administrative Office Orders/Decisions and Guidelines as approved by Institutes' Governing Body i.e. Board of Governors (BOG)</li>
+                
+            </ol>
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.12 Statement of the categories of
+documents held by the organization or under its control</td>
+        <td style="width: 80%;text-align:justify;"><a class="link" href="http://old.nitdgp.ac.in/RTI/4.13Organization Chart.pdf">Organization PDF form ( Organization Chart was approved by the 42nd Board of Governors Meeting)</a></td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.14 Council, Board, Committees/Governing body of the Institute</td>
+        <td style="width: 80%;text-align:justify;"> 
+            <ol type="i">
+                <li><a class="link" href="http://old.nitdgp.ac.in/RTI/4.14 i) BOG Members.pdf">Board of Governors</a></li>
+                <li><a class="link" href="http://old.nitdgp.ac.in/RTI/4.14 ii)Finance Committee members.pdf">Finance Committee</a></li>
+                <li><a class="link" href="http://old.nitdgp.ac.in/RTI/4.14 iii) B&amp;WC Member.pdf">Building and Works Committee</a></li>
+                <li><a class="link" href="http://old.nitdgp.ac.in/RTI/4.14 iv)Senate Member list.pdf">Senate</a></li>
+            </ol>
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.15 A directory of its faculty, officers and employees</td>
+        <td style="width: 80%;text-align:justify;">Institute Telephone Directory</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.16 The monthly remuneration received by each of its faculty, officers and employees as provided in its regulation.</td>
+        <td style="width: 80%;text-align:justify;"><a class="link" href="http://old.nitdgp.ac.in/RTI/4.16 The monthly remuneration received by each of its faculty, officers and employees as provided in its regulation..pdf">Pay Structure of Institute Employees</a></td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;"> 4.17 The budget allocated to each of its agency, indicating the particulars of all plans, proposed expenditures and reports on disbursements made.</td>
+        <td style="width: 80%;text-align:justify;">Data of Budgetary Allocation &amp; Expenditure of all plans.</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.18 The manner of execution of subsidy programmes, including the amounts allocated and the details of beneficiaries of such programmes.</td>
+        <td style="width: 80%;text-align:justify;">No Subsidy is given</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.19 Particulars of recipients of concessions, permits or authorizations granted by it.</td>
+        <td style="width: 80%;text-align:justify;">Faculty chairs and its occupants Scholarships</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;">4.20 Details in respect of the information available to or held by it reduced in an electronic form.</td>
+        <td style="width: 80%;text-align:justify;">Please See Institute Website: www.nitdgp.ac.in</td>
+    </tr>
+    <tr>
+        <td style="width: 20%;text-align:justify;"> 4.21 The particulars of facilities available to citizens for obtaining information, including the working hours of a library or reading room , if maintained for public use.</td>
+        <td style="width: 80%;text-align:justify;">Central Library is not available for public use.</td>
+    </tr>
+    </tbody></table><br/><br/>
+    <table class="table table-bordered">
+    <tbody><tr><td colspan="2"><h3>5. Statement of categories of documents held by the organisation or under its control :</h3></td></tr>
+    <tr>
+        <td style="width: 80%;text-align:justify;">5.1 Minutes of the meeting of Board of Governors</td>
+        <td style="width: 20%;text-align:justify;"><a href="/bog">View Details..</a></td>
+    </tr>
+    <tr>
+        <td style="width: 80%;text-align:justify;"> 5.2 Minutes of the meeting of Finance Committee </td>
+        <td style="width: 20%;text-align:justify;"><a href="/bwcifc">View Details..</a></td>
+    </tr>
+    <tr>
+        <td style="width: 80%;text-align:justify;">5.3 Minutes of the meeting of Building &amp; Works committee </td>
+        <td style="width: 20%;text-align:justify;"><a href="/bwcifc">View Details..</a></td>
+    </tr>
+    <tr>
+        <td style="width: 80%;text-align:justify;">5.4 Minutes of the meeting of the Senate </td>
+        <td style="width: 20%;text-align:justify;"><a href="/senate">View Details..</a></td>
+    </tr>
+    <tr>
+        <td style="width: 80%;text-align:justify;">5.5 Annual Reports of the Institute</td>
+        <td style="width: 20%;text-align:justify;"><a href="/reports">View Details..</a></td>
+    </tr>
+    </tbody></table><br/><br/>
+    <table class="table table-bordered">
+    <tbody><tr><td colspan="2"><h3>6. Particulars of any arrangement that exists for consultation with, or representation by the members of the public in relation to the formulation of its policy or implementation thereof:</h3></td></tr>
+    <tr>
+        <td style="width: 80%;text-align:justify;" colspan="2">Public representation in formulation of policy and its implementation is ensured through Board of Governors, Finance Committee and Building and Works Committee of the Institute which are represented by members from different fields such as academics, industries and government. These committees are the highest policy making authorities of the Institute. The Senate, the highest academic body of the Institute is also represented by members from academics and industries.</td>
+       
+    </tr>
+    </tbody></table><br/><br/>
+    <table class="table table-bordered">
+    
+    <tbody><tr>
+        <td style="width: 80%;text-align:justify;"><h3>7. Budget allocated to each of its agency, indicating the particulars of all plans, proposed expenditures and reports on disbursements made:</h3></td>
+        <td style="width: 20%;text-align:justify;"><a href="http://old.nitdgp.ac.in/information/annual_accounts.php">View Details..</a></td>
+    </tr>
+    <tr>
+        <td colspan="2" style="text-align:justify;">Provided in the Annual Accounts Reports of the Institute which is available in the Institute Website : www.nitdgp.ac.in</td>
+    </tr>
+     </tbody></table><br/><br/>
+      <!-- STATIC CONTENT ENDS HERE -->
+
+
+      <h3>8. RTI Qraterly Return Form</h3>
       <ul class="list-group list-gr">
         <li v-for="rti in rtis">
           <a class="list-group-item" target="new" :href="rti.file">
@@ -41,3 +261,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+td {
+  color: black
+}
+</style>
+
