db09d60ebbc4890369b85ab16927eb8af75aa7e9
Server-Room <support@mail1.nitdgp.ac.in>
2018-10-06 11:22:32 +0530

    http added before href link

--- frontend/src/components/information/Pension.vue
+++ frontend/src/components/information/Pension.vue
@@ -3,17 +3,17 @@
     <card title="Pensioners' Corner">
       <ul class="list-group list-gr">
         <li>
-          <a class="list-group-item" target="new" href="nitdgp.ac.in/AllPDF/Circular Dated 06.11.2015.pdf">Circular Dated 06.11.2015</a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/Circular Dated 06.11.2015.pdf">Circular Dated 06.11.2015</a>
         </li>
         <li>
-          <a class="list-group-item" target="new" href="nitdgp.ac.in/AllPDF/Life Certificate.pdf">Pensioner's/Family Pensioner's Life Certificate</a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/Life Certificate.pdf">Pensioner's/Family Pensioner's Life Certificate</a>
         </li>
         <li>
-          <a class="list-group-item" target="new" href="nitdgp.ac.in/AllPDF/Pro Forma for Restoration of Commuted Portion of Pension After 15 Years from the Date of Commutation.pdf">Pro Forma for Restoration of Commuted Portion of Pension After 15 Years from the Date of Commutation</a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/Pro Forma for Restoration of Commuted Portion of Pension After 15 Years from the Date of Commutation.pdf">Pro Forma for Restoration of Commuted Portion of Pension After 15 Years from the Date of Commutation</a>
         </li>
         <li>
-          <a class="list-group-item" target="new" href="nitdgp.ac.in/AllPDF/Pro Forma for Additional Quantum of Pension.pdf">Pro Forma for Additional Quantum of Pension</a>
-          <a class="list-group-item" target="new" href="nitdgp.ac.in/AllPDF/pensioner conner0001.pdf">FMA Undertaking Format </a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/Pro Forma for Additional Quantum of Pension.pdf">Pro Forma for Additional Quantum of Pension</a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/pensioner conner0001.pdf">FMA Undertaking Format </a>
         </li>
       </ul>
     </card>
