bc115a405a3230a5daaf8ea65156e88b43a323d0
Server-Room <support@mail1.nitdgp.ac.in>
2018-11-01 19:50:10 +0530

    GeneralUrl import from common js

--- frontend/src/components/facilities/Question.vue
+++ frontend/src/components/facilities/Question.vue
@@ -14,6 +14,7 @@
 import axios from 'axios'
 import Card from "@/components/Card"
 import LinksPage from "@/components/LinksPage"
+import { genBackendURL } from '@/common.js'
 
 export default {
   name: "Question",
