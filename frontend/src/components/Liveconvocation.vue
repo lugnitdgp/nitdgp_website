573e8c7804c29178274891314d601526fd71a77f
Hasibul <hmondal236@gmail.com>
2018-11-11 00:16:04 +0530

    Title and link modified in Liveconvocation

--- frontend/src/components/Liveconvocation.vue
+++ frontend/src/components/Liveconvocation.vue
@@ -1,6 +1,6 @@
 <template>
   <links-page>
-    <card title="Liveconvocation">
+    <card title="Live Convocation">
       <div align="center">
       <iframe width="1280" height="720" src="https://www.youtube.com/embed/p1vteg3c4zo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
@@ -9,7 +9,6 @@
 </template>
 
 <script>
-import axios from 'axios'
 import LinksPage from '@/components/LinksPage'
 import Card from '@/components/Card'
 
@@ -17,15 +16,12 @@ export default {
   name: 'Liveconvocation',
   data() {
     return {
-      visitor: {
-      name: 'President of India page',
-      link: 'http://presidentofindia.nic.in/profile.htm'
-      }
+      
     }
   },
   created () {
     this.$emit('hideloader', true)
-    // window.location = 'http://presidentofindia.nic.in/profile.htm'
+    
   },
   components: {
     LinksPage,
