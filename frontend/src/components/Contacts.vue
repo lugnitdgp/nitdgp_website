27ebbdb0ee707d492441daecae4aedd08e3d1a71
Compro-Prasad <comproprasad@gmail.com>
2018-11-24 12:11:42 +0530

    Removed an unecessary comment from Contacts.vue

--- frontend/src/components/Contacts.vue
+++ frontend/src/components/Contacts.vue
@@ -532,16 +532,6 @@
                   <span v-html="details.contact"/>
                 </li>
               </div>
-              <!-- <br>xxxxxxxxxxxxxx<br>
-                   <div v-for="details in list" class="col contact-col ">
-                   <li class="list-group-item disabled" style="height: 100%">
-                   <h4>{{ details.name }}</h4>
-                   <h5 style="font-weight: bold" v-if="details.designation">
-                   {{ details.designation }}
-                   </h5>
-                   <span v-html="details.contact"/>
-                   </li>
-                   </div> -->
             </div>
           </ul>
         </div>
