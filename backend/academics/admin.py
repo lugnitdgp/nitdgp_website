38cd928a04e1281638f754fe43258f229a19e463
Compro-Prasad <comproprasad@gmail.com>
2018-11-24 12:08:53 +0530

    Added support for multiple admins for different notice types(groups)
    Just create the following groups in django admin(as it is):
    - Academic Notice
    - General Notice
    - Student Notice
    - Hostel Notice
    
    Then assign the specific group roles to specific users.

--- backend/academics/admin.py
+++ backend/academics/admin.py
@@ -3,7 +3,26 @@ from academics.models import *
 
 
 class NoticeModelAdmin(admin.ModelAdmin):
-    list_display = ['__str__', '_notice_type', '_file', '_date']
+    list_display = ['title', 'notice_type', 'date']
+    list_editable = ['notice_type']
+    list_filter = ['notice_type']
+    search_fields = ['title', 'notice_type']
+
+    def get_queryset(self, request):
+        queryset = super(NoticeModelAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        groups = request.user.groups.filter(name__endswith=" Notice")
+        notice_types = [x.name.split()[0] for x in groups]
+        return queryset.filter(notice_type__in=notice_types)
+
+    def formfield_for_choice_field(self, db_field, request, **kwargs):
+        if db_field.name == "notice_type" and not request.user.is_superuser:
+            groups = request.user.groups.filter(name__endswith=" Notice")
+            kwargs['choices'] = (
+                (x.name.split()[0], x.name.split()[0]) for x in groups
+            )
+        return super().formfield_for_choice_field(db_field, request, **kwargs)
 
 
 class AdmissionDegreeModelAdmin(admin.ModelAdmin):
