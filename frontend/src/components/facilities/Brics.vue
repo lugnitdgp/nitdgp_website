c4213783d7a4e0032e2316d6d6abbad0a4116046
Server-Room <support@mail1.nitdgp.ac.in>
2019-02-07 19:05:53 +0530

    Containt of BRICS added

--- frontend/src/components/facilities/Brics.vue
+++ frontend/src/components/facilities/Brics.vue
@@ -1,24 +1,230 @@
 <template>
   <links-page>
-    <card title="BRICS">
-      <notice-list :noticelist="bricslist" />
+    <card title="Brazil, Russia, India, China and South Africa [BRICS]">
+      <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">BRICS</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              Recently NIT Durgapur steps into the BRICS Research forum. BRICS is composed of Brazil, Russia, India, China and South Africa, and accounts for 26.46% of the world territory and 42.58% of the world population. Based upon 10 years' development, BRICS, as an mportant platform for emerging markets and developing countries, has evolved into an important driving force to global economic growth and trade cooperation, as well as to global economic governance reform. BRICS countries are important players in science and technology competition on the world arena, and they would facilitate their domestic STI development with the multilateral platform. The MOU on STI Cooperation adopted in 2015 is guiding overall cooperation covering sustainable agriculture, food security, natural disasters, water resources and pollution treatment, new and renewable energy, energy efficiency, space research, geospatial technology, medicine and life sciences, new materials and nanomaterials, photonics, information and communication technology, ocean and polar sciences, etc.
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">BRICS Programme at NIT Durgapur</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              <span style="color: green;font-weight: bold;">About NIT Durgapur:</span><br>The National Institute of Technology Durgapur [NIT Durgapur, formerly known as Regional Engineering College, Durgapur] was established in 1960. NIT Durgapur is an Institute of National Importance under the financial assistance from Department of Higher Education, Ministry of Human Resource Development, Government of India. It is an autonomous institute. The institute offers B. Tech., M. Tech., M. Sc., MCA and MBA degree and Ph D degree in the following disciplines:<br>
+              <span style="color: green;font-weight: bold;">Engineering Departments:</span> Biotechnology, Computer Science & Engg, Chemical Engineering, Civil Engineering, Electronics & Communication Engineering, Electrical Engineering, Mechanical Engineering, Metallurgical & Materials Engg, <br>
+              <span style="color: green;font-weight: bold;">Science Departments:</span> Physics, Chemistry, Mathematics, Earth & Env. Studies, Humanities, Management Studies<br>
+              <table class="table table-responsive-md table-bordered">
+                <tbody>
+                  <tr>
+                    <td>Faculty strength</td>
+                    <td>230</td>
+                  </tr>
+                  <tr>
+                    <td>Total student strength</td>
+                    <td>6000</td>
+                  </tr>
+                  <tr>
+                    <td>Undergraduate intake</td>
+                    <td>970</td>
+                  </tr>
+                  <tr>
+                    <td>Post graduate intake</td>
+                    <td>514</td>
+                  </tr>
+                  <tr>
+                    <td>Ph D courses intake</td>
+                    <td>120</td>
+                  </tr>
+                  <tr>
+                    <td>International students</td>
+                    <td>57</td>
+                  </tr>
+                </tbody>
+              </table>
+              <br>
+              <span style="color: #000;font-weight: bold;">(Foreign Students who are nominated by the Ministry of External Affairs, Government of India, and the Indian Council for Cultural Relations, Government of India get admission in this institute).</span><br>
+              Website : <span style="color: green;font-weight: bold;"><a href="https://nitdgp.ac.in"></a>www.nitdgp.ac.in</span><br>
+              <h3>Involvement of NIT Durgapur in the thematic area under BRICS Programme:</h3><br>
+              <span style="color: green;font-weight: bold;">Water Resources and Pollution Treatment</span><br>
+              <h3>Description of the educational programmes, which the university offers to the partner NU universities in the framework of the thematic priorities of BRICS NU</h3><br>
+              <span style="color: green;font-weight: bold;">PhD programmes:</span><br>
+              PhD programmes offered by Department of Physics, Civil Engg, Chemical Engg, Earth & Environmental Studies, Chemistry, and Biotechnology in the areas of Water resource and Pollution control. The programme is multi-disciplinary in nature. NIT Durgapur has supporting infrastructure for exchange of international students. Presently a good number of students from abroad are in NIT Durgapur for perusing B. Tech., M. Tech and Ph D Programme.
+            </div><br>
+            <collapse-list>
+            <card-collapse  title="Contact information of the people, responsible for the work with the BRICS Network University">
+            <div style="background-color: #f2f2f2;">
+              <div class="carousel-inner person-list" role="listbox">
+                  <div class="carousel-item active">
+                    <div class="row">
+              <div class="col testimonial" >
+                <div class="card testimonial-card text-center">
+                  <div class="card-up">
+                  </div>
+                  
+                  <div align="center">
+                    <img src="https://admin.nitdgp.ac.in/files/faculty/Anupam%20Basu/anupambasu.png" class="avatar mx-auto rounded-circle img-fluid">
+                  </div>
+                  <div class="card-body-in">
+                    <p class="card-title">
+                      <strong>
+                        <a class="blue-text notranslate" href="https://nitdgp.ac.in/faculty/2c36b313-cfa6-47bf-b1fa-9b2b226fa8f0" target="_blank">
+                          Prof. (Dr.) Anupam Basu
+                        </a>
+                      </strong>
+                    </p>
+                    
+                    <h6 class="black-text">Director<br>National Institute of Technology Durgapur (NIT Durgapur),India <br>ITG member, BRICS Programme</h6>
+                    <hr>
+                    <p class="min-profile">
+                      <i class="fa fa-envelope"></i>&nbsp;&nbsp;&nbsp;director@admin.nitdgp.ac.in
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col testimonial">
+                <div class="card testimonial-card text-center">
+                  <div class="card-up">
+                  </div>
+                  
+                  <div align="center">
+                    <img src="https://admin.nitdgp.ac.in/files/faculty/Supriya%20Pal/supriya_pal.jpg" class="avatar mx-auto rounded-circle img-fluid">
+                  </div>
+                  <div class="card-body-in">
+                    <p class="card-title">
+                      <strong>
+                        <a class="blue-text notranslate" href="https://nitdgp.ac.in/faculty/3bb0850d-99fb-4c97-8c24-1fccd9da10de" target="_blank">
+                          Dr. Supriya Pal
+                        </a>
+                      </strong>
+                    </p>
+                    
+                    <h6 class="black-text">Associate Professor<br> Department of Civil Engineering and Coordinator, (NIT Durgapur),India<br>ITG member, BRICS Programme</h6>
+                    <hr>
+                    <p class="min-profile">
+                      <i class="fa fa-envelope"></i>&nbsp;&nbsp;&nbsp;supriya070478@gmail.com<br>
+                      <i class="fa fa-phone"></i>&nbsp;&nbsp;&nbsp; +919434788154
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col testimonial">
+                <div class="card testimonial-card text-center">
+                  <div class="card-up">
+                  </div>
+                  
+                  <div align="center">
+                    <img src="https://admin.nitdgp.ac.in/files/faculty/Mrinal%20Kanti%20Mandal/mrinalkanti_mandal.jpg" class="avatar mx-auto rounded-circle img-fluid">
+                  </div>
+                  <div class="card-body-in">
+                    <p class="card-title">
+                      <strong>
+                        <a class="blue-text notranslate" href="https://nitdgp.ac.in/faculty/16fb31f5-59f5-46b6-b31d-bb0c1575eb44" target="_blank">
+                          Dr. Mrinal Kanti Mandal
+                        </a>
+                      </strong>
+                    </p>
+                    
+                    <h6 class="black-text">Associate Professor<br>Department of Chemical Engineering,NIT Durgapur, India</h6>
+                    <hr>
+                    <p class="min-profile">
+                    <i class="fa fa-envelope"></i>&nbsp;&nbsp;&nbsp;mrinalmandal@gmail.com
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col testimonial">
+                <div class="card testimonial-card text-center">
+                  <div class="card-up">
+                  </div>
+                  
+                  <div align="center">
+                    <img src="https://admin.nitdgp.ac.in/files/faculty/Hirok%20Chaudhuri/55.JPG" class="avatar mx-auto rounded-circle img-fluid">
+                  </div>
+                  <div class="card-body-in">
+                    <p class="card-title">
+                      <strong>
+                        <a class="blue-text notranslate" href="https://nitdgp.ac.in/faculty/bed09767-49b2-4ed9-a5d8-aaefbeea9a40">
+                          Dr. Hirok Chaudhuri
+                        </a>
+                      </strong>
+                    </p>
+                    
+                    <h6 class="black-text">Associate Professor<br>Department of Physics, Single Point of Contact to Ministry of HRD, Govt. of India for BRICS Programme at NIT Durgapur and ITG member, BRICS Programme NIT Durgapur, India,</h6>
+                    <hr>
+                    <p class="min-profile">
+                      <i class="fa fa-envelope"></i>&nbsp;&nbsp;&nbsp;chaudhuri_hirok@yahoo.co.in
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+            </div>
+          </card-collapse>
+          <card-collapse title="About BRICS NUs and BRICS ITG on “Water Resources and Pollution Treatment”">
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              BRICS Network Universities (BRICS NUs) was developed in the BRICS 7th summit in Russia in July 2015, under a Russian proposal of the establishment of BRICS NU, which was written into Ufa Declaration. The International Thematic Group (ITG) purpose is to develop theoretical and practical knowledge about water resources and the methods of pollution treatment. In particular, the ITG is aimed at:
+              <ol>
+                <li>developing joint educational and professional training programs on protection of water resources and pollution treatment tailored to the BRICS countries;</li>
+                <li>conducting research on the formation of water resources and the origin of groundwater and surface water composition in the BRICS countries;</li>
+                <li>developing rational methods of natural and wastewater treatment and water conservation.</li>
+              </ol>
+              Member Universities of BRICS NUs
+              <ol>
+              <li>Federal University of Minas Gerais (Brazil)</li>
+              <li>Federal University of Rio de Janeiro (Brazil)</li>
+              <li>Indian Institute of Technology Kanpur (India)</li>
+              <li>National Institute of Technology Durgapur (India)</li>
+              <li>Hohai University (China)</li>
+              <li>North China University of Water Resources and Electric Power (China)</li>
+              <li>Durban University of Technology (South Africa)</li>
+              <li>Central University of Technology (South Africa)</li>
+              <li>Saint Petersburg State University (Russia)</li>
+              <li>Tomsk Polytechnic University (Russia)</li>
+              </ol>
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">BRICS NU ITG Meeting (2018) South Africa</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              <h4>NIT Durgapur in BRICS Network University (NU)</h4><br><h5>Water Resources and Pollution Treatment</h5>
+              Participating Institutions:
+              <ol>
+                <li>Federal University of Rio de Janeiro (Brazil)</li>
+                <li>Tomsk Polytechnic University (Russia)</li>
+                <li>Natiaonal Institute of Technology Durgapur (India)</li>
+                <li>Hohai University (China)</li>
+                <li>Durban University of Technology (South Africa)</li>
+              </ol>
+            </div>
+          </card-collapse>
+          <card-collapse title="Plan Of NIT Durgapur">
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              <p>Recently three faculty members from Department of Physics, Department of Civil Engineering and Department of Chemical engineering at NIT Durgapur started a joint research projects on Water Pollution treatment with some premier universities of Russia and China under BRICS STI Framework. The research group focused on the degradation of organic pollutants from industrial wastewater of Durgapur area using advance oxidation process, photocatalytic membrane reactor. Foreign collaborations may facilitate to open up a new avenue for the researchers of NIT Durgapur in the wastewater treatment technology.</p>
+              <p>Researchers from NIT Durgapur is working on water management and pollution treatment for quite sometime. Different faculty members, Research Students, M. Tech and B. Tech students of various department of NIT Durgapur such as Chemistry, Chemical Engineering, Civil Engineering, Earth and Environmental Studies, Computer Science and Engineering, Biotechnology and Physics join hands to work on the burning issue on water management and pollution treatment. NIT Durgapur is also going to launch Center for Environment and Water (CREW). This center will provide opportunity to the researchers of NIT Durgapur and its associates to work together in the field of Water and Environmental research such environmental radioactivity monitoring, air pollution, soil pollution, wastewater treatment using biological process and membrane process and many more.</p>
+              <p>A group of researchers from department of Physics, Civil Engineering,computer science and engineering and Chemical engineering along with CDAC,Kolkata are also planning for new arena of research on sensor based automated water management system of the river Damodar. This may facilitate towards proper allocation and utilization of water for the agriculture sectors, industries and municipal corporations of under the Asansole Durgapur Development Authority as well as three districts of West Bengal – Purba Bardhaman, Paschim Bardhaman and Bankura.</p>
+            </div>
+          </card-collapse>
+          <card-collapse title="Remaining Details of BRICS">
+            <card>
+              <notice-list :noticelist="bricslist" />
+            </card>
+          </card-collapse>
+        </collapse-list>
     </card>
   </links-page>
 </template>
 
 <script>
 import axios from 'axios'
+import NoticeList from '@/components/NoticeList'
 import LinksPage from '@/components/LinksPage'
 import Card from '@/components/Card'
-import NoticeList from '@/components/NoticeList'
+import CollapseList from '@/components/CollapseList'
+import CardCollapse from '@/components/CardCollapse'
 import { genBackendURL } from '@/common.js'
 
 export default {
   name: "BRICS",
   data () {
     return {
-      bricslist: [],
-      paginate: ['bricslist']
+      bricslist: []
     }
   },
   created () {
@@ -34,7 +240,51 @@ export default {
   components: {
     LinksPage,
     Card,
+    CollapseList,
+    CardCollapse,
     NoticeList
   }
 }
 </script>
+<style scoped>
+  .col.testimonial {
+    min-width: 200px;
+    min-height: 100px;
+    max-width: 320px;
+    max-height: 500px;
+    padding: 7px;
+  }
+  .min-profile {
+    font-size: 72% !important;
+  }
+  hr {
+    margin-left: 2em !important;
+    margin-right: 2em !important;
+  }
+  .card-body-in .card-title .notranslate {
+    font-size: 88% !important;
+  }
+  .card-body-in h6{
+    font-size: 80% !important;
+  }
+  .testimonial-card .desig{
+    font-size: 100%;
+  }
+  .testimonial-card .card-title{
+    font-size: 100%;
+  }
+  .testimonial-card {
+    min-height: 100% !important;
+    min-width: 100% !important;
+  }
+  .card-up {
+    height: 80px !important;
+  }
+  .card-up {
+    background: linear-gradient(to right,#001333,#324e7d) !important;
+  }
+  .avatar {
+    height: 100px !important;
+    width: 100px !important;
+  }
+</style>
