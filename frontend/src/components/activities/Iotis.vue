46ba3f130a009a893d9f622769ef8cbb42852e25
Server-Room <support@mail1.nitdgp.ac.in>
2019-02-08 14:57:44 +0530

    IoTIS page updated

--- frontend/src/components/activities/Iotis.vue
+++ frontend/src/components/activities/Iotis.vue
@@ -1,11 +1,198 @@
 <template>
   <links-page>
-    <card title="IOTIS">
-    	<h3>Page Under Construction</h3>
+    <card title="Centre of Excellence on IoT and Intelligent Systems(IoTIS)">
+    	<div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Introduction</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">The mission of the Centre of excellence for 'Advanced Research on IoT and Intelligent Systems' is to study, explore, design, develop, and study reliable, scalable and self-organized networks and systems leading to ubiquitous computing that offers an inclusive, sustainable and caring ambiance for living.  In recent years, we’ve all been hearing a lot about smart cites, smart homes and the Internet of Things (IoT).  We live in a world where Internet connectivity and smart phone ownership is near ubiquitous. Underlying and complementing these factors is the IoT. Now a days, we are surrounded by hundreds of smart but possibly ‘passive’ (lacks sufficient intelligence) devices and are able to enjoy mere ubiquitous connectivity among them. The journey ahead towards ‘smart space’ in true sense is possible only if, at first, we can convert millions of these passive pervasive devices to ubiquitous intelligent ‘smart objects’ which are able to sense locally in real-time, take decision independently (or in coordination) and act effectively to partially take over complex human perceptive & cognitive functions. Secondly, seamlessly integrate their collective pervasive intelligence to act unnoticeable in the background to achieve a desired task as a unified system. Presently at NIT Durgapur, we have many faculty members from different Departments with a wealth of knowledge and potential in various areas of IoT and Machine learning.  Over the past several years these researchers have continually demonstrated their productivity through their ability to publish in scientific journals, actively participate in national and international conferences, and secure external funding for their research.  NIT Durgapur is now in a position to establish a centre for IoT and Machine Learning that will provide a dynamic environment for faculty to expand their research and attract talented postgraduate students and research scholars.  There is a compelling need to solve complex social and biomedical problems that are crucial to human welfare and society in general and that requires more broadly trained scientists with multidisciplinary perspectives that include engineering sciences, physical sciences, mathematics, and biological sciences.
+
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">About the Centre</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              The proposed ‘Centre of Excellence for Advanced Research on IoT and Intelligent Systems’ (IoTIS) will focus on advanced interdisciplinary research activities in the area of Internet of Things (IoT) and assistive technologies related to Intelligent Systems to cater to the needs of the society. Another goal of IoTIS is to meet the challenges of the nation by encouraging research and entrepreneurship in interdisciplinary areas, encompassing engineering, sciences, and social sciences. IoTIS also aims to reach out to universities, research institutions and industrial organizations with periodic continuing-education programs for proper utilization of national resources. As far as academic programs are concerned, the centre will be offering Masters, and PhD programs in Advanced Networking and Intelligent Systems. Going forward, the centre seeks collaboration with national and international institutes and partnership with social institutions and industries to realize its goals. 
+
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Vision & Mission</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              <span style="color: green;">Vision :&nbsp;</span>To be a globally recognised centre of excellence in the field of IoT and Machine learning providing/imparting quality education, interdisciplinary research and innovative development of smart systems.<br>
+              <span style="color: green;">Mission : &nbsp;</span>
+              <ul>
+              <li>To create a fusion of Networking Systems and Intelligent Systems that promotes scientific discovery and the invention of smart and intelligent systems technologies.</li> 
+              <li>Interdisciplinary research and education to nurture future experts in IoT research by imparting interdisciplinary knowledge, innovative ideas, critical thinking and practical skills.</li>
+              <li>To undertake cutting edge research and education in the areas of networking systems and computational intelligence, and to derive evolving, comprehensive and integrative methods for solving various societal problems to make an impact on sustainable living.</li>
+              <li>To be the premier source of well-educated network and IoT engineers, fostering strong academic, industrial, and social ties, and thereby achieving significant societal impact.</li>
+              <li>To produce the trained manpower required in the social sector development program.</li>
+              <li>To carry out innovative research and development that contributes to improve social impacts in the Country. </li>
+              <li>To build the capacity of technical personnel working in the various institutions.</li>
+              <li>To provide consultancy, training, and outreach services for stakeholders in the region including social institutions.</li>
+            </ul>
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Goals/Objectives</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              To realize its vision and accomplish its mission, the centre will have the following specific objectives:
+              <ul>
+            <li>To carry out advanced interdisciplinary research activities in the area of Internet of Things (IoT) and related Intelligent Systems (and Smart Systems) to cater to the needs of the industry and society (in general).</li>
+            <li>Conducting training programmes in the related areas for professional skill development.</li>
+            <li>Generating Intellectual Properties (IP) in terms of patents and high quality technical publications.</li>
+            <li>Conducting consultancy works for industries covering, but not limited to, transportation, manufacturing, healthcare, agriculture etc.</li>
+            <li>Patenting and transfer of technology</li> 
+            <li>Helping and guiding start-ups in relevant areas.</li>
+            <li>To conduct courses with significant participation of external faculty, industrial and regulatory experts on IoT and machine Learning.</li>
+            <li>Offer PhD courses in relevant areas of expertise. Also, to provide customized/flexible PhD Courses to student(s) of any department of the institute pursuing/ intended to pursue his/her thesis on specific problems in IoT and machine Learning.</li>
+            <li>To enter into collaborative research engagements with Extra-mural funding agencies and Industrial Organizations for better understanding of issues of scientific and technological management between different stakeholders.</li>
+            <li>Using this as a general research facility to take up new research projects from industry and other bodies.</li>
+
+              </ul>
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Tangible deliverables</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              <ul>
+              <li>Patents, papers and technology transfer.</li>
+              <li>High quality man power with adequate skill to meet the new challenges evolving out of rapid technology changes.</li>
+              <li>System prototype implementation for specific areas. E.g. challenged network scenarios, intelligence, assistive needs, agriculture, traditional handicrafts and other industries on the verge of extinctions due to pressure from big players.</li>
+              <li>High quality man power with adequate skill to meet the new challenges evolving out of rapid technology changes.</li>
+              <li>Generate revenue form consultancy, technology transfer and training programmes.</li>
+            </ul>
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Partners and Stakeholders</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              The following are the major partners and stakeholders of the Centre of IoT and Machine Learning in its teaching and research activity<br><br>
+              <h5>Potential Partners</h5>
+              <ul>
+                <li>Central Mechanical Engineering Research Institute (CMERI), Durgapur.</li>
+                <li>Indian Institute of Technology, Khragpur.</li.>
+                <li>Indian Institute of Technology, Bombay.</li>
+                <li>Jadavpur University.</li>
+                <li>Indian Institute of Engineering, Science & Technology, Shibpur</li>
+              </ul>
+                <h5>Stakeholders</h5>
+                <ul>
+                <li>Ministry of Human and Resource Development, Govt. of India</li>
+                <li>Ministry of Housing and Urban Affairs, Govt. of India </li>
+                <li>Ministry of Information Technology, Govt. of India</li>
+                <li>Durgapur Municipal Corporation, Durgapur</li>
+                <li>Different Departments of National Institute of Technology Durgapur</li>
+              </ul>
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Benefits</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+
+              <h5>Students will,</h5>
+              <ul>
+              <li>gain broad exposure to IoT research;</li> 
+              <li>interact with faculty and students across different disciplines;</li> 
+              <li>learn techniques that address IoT research questions;</li> 
+              <li>experience the synergy of an interdisciplinary approach to understanding Intelligent systems;</li> 
+              <li>develop an appreciation for interrelationships among the disciplines;</li> 
+              <li>be eligible to receive start-up funds (through Alumni); </li>
+              </ul>
+            <h5>Faculty members will,</h5>
+            <ul>
+            <li>have access to core facilities and shared equipment;</li> 
+            <li>have the opportunity to work in a highly collaborative research environment; </li> 
+            <li>be eligible to receive support for attending national meetings and professional development programs/workshops in the relevant areas</li> 
+            <li>be able to strengthen the depth and breadth of their research projects through interdisciplinary research, which will enhance their ability to secure external funding. </li>
+            </ul>
+            </div><br>
+
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Focus Area of Research</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              The proposed research areas are shown in the details in Fig. 1. The possible research activities under each layer is initially proposed below:
+              <ul type="square">
+                <li>IoT Cloud</li>
+                <ul>
+                  <li>Clouds as service provisioning, repository and virtualization platform</li>
+                  <li>Efficient cloud resource management through AI and learning</li>
+                  <li>Analytics on acquired data for patterns, prediction and applications.</li>
+                  <li>Cloud level security, authentication and forensics.</li>
+                </ul>
+                <li>Middleware</li>
+                <ul>
+                  <li>Interface of low capability devices with clouds (with context awareness)</li>
+                  <li>Middleware design distributed processing and QoS/QoE</li>
+                  <li>Context aware subsystems integration for managing device level diversities.</li>
+                  <li>Middleware security algorithms</li>
+                </ul>
+                <li>Devices</li>
+                <ul>
+                  <li>Application specific device modelling.</li>
+                  <li>Robotics with embedded AI and machine learning for specific application areas like manufacturing and healthcare.</li>
+                  <li>Embedded systems for grass root level activities</li>
+                  <li>Communication interfaces and other related technologies for sensors and actuators</li>
+                  <li>Design and modelling of scenario/application specific IoT subsystems (E.g. smart home, smart vehicle, mission specific swarm robotics etc.)</li>
+                  <li>Device and subsystem level light weight security including hardware.</li>
+
+                </ul>
+            </ul>
+
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Sustainability</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              <ul>
+              <li>Special Manpower Development leading to Masters and PhD degrees.</li>
+              <li>Technology Incubation for Start-up.</li>
+              <li>Transfer of patented technology.</li> 
+              <li>Training, Short Term courses, Continuing education and Consultancy.</li>
+              <li>Vetting projects in Industry and Funding Agencies.</li> 
+            </ul>
+              <img :src="slides[0].image" alt="slides[0].primary_caption" class="img img-responsive " width="100%">
+            </div><br>
+            
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">IoTIS Committees</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              Resolutions of the meeting of IoTIS group held on 31.10.2018 for formation of Academic Committee, execution committee and intial action plan. The resolutions are listed below. <br>
+              <ul type="square">
+              <span><h4><li>Academic Committee:</li></h4></span><br>
+                <ol>
+                  <li>Subhrabrata Choudhury(CSE) &nbsp;&nbsp;&rarr;&nbsp;Coordinator</li>
+                  <li>Tandra Pal(CSE) &nbsp;&nbsp;&rarr;&nbsp;Member</li>
+                  <li>Prasenjit Choudhury(CSE) &nbsp;&nbsp;&rarr;&nbsp;Member</li>
+                  <li>Aniruddha Chandra(ECE) &nbsp;&nbsp;&rarr;&nbsp;Member</li>
+                  <li>Chiranjib Koley(EE) &nbsp;&nbsp;&rarr;&nbsp;Member</li>
+                  <li>Nirmal Baran Hui(ME) &nbsp;&nbsp;&rarr;&nbsp;Member</li>
+                  <li>Dean Academic Member &nbsp;&nbsp;&rarr;&nbsp;(ex-officio)</li>
+                  <li>Dean Reserach & Consultancy &nbsp;&nbsp;&rarr;&nbsp;Member (ex-officio)</li>
+                </ol><br>
+                <span><h4><li>Executive Committee for centre of excellence on IoTIS</li></h4></span>
+                <ol>
+                  <li>Anirban Sarkar(CSE)&nbsp;&nbsp;&rarr;&nbsp;Convener</li>
+                  <li>Sajal Mukhopadhyay(CSE)&nbsp;&nbsp;&rarr;&nbsp;Memeber</li>
+                  <li>Animesh Dutta(CSE)&nbsp;&nbsp;&rarr;&nbsp;Memeber</li>
+                  <li>Bibhas Sen(CSE)&nbsp;&nbsp;&rarr;&nbsp;Memeber</li>
+                </ol>
+                
+              </ul>
+            </div><br>
+            <div style="background-color: #001333;color: #fff;padding: 10px;text-align: center;">Remaining Informations of IoTIS</div>
+            <div style="border: 2px solid #001333;padding: 10px;text-align: justify;">
+              <ul class="list-group list-gr">
+                <li>
+                  <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/IoTIS/The Team_IoTIS.pdf">The Team </a>
+                </li>
+                <li>
+                  <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/IoTIS/Projects at a Glance_IoTIS.pdf">Projects at a Glance </a>
+                </li>
+                <li>
+                  <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/IoTIS/Detailed Budget Estimate_IoTIS.pdf">Budget Summary & Detailed Budget Estimate </a>
+                </li>
+                <li>
+                  <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/IoTIS/Activity Timeline_IoTIS.pdf">Activity Timeline</a>
+                </li>
+                <li>
+                  <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/IoTIS/Project Monitoring_IoTIS.pdf">Project Monitoring and Steering Committee</a>
+                </li>
+                <li>
+                  <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/IoTIS/Publications_IoTIS.pdf">Selected List of Publications in the relevant area</a>
+                </li>
+                <li>
+                  <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/IoTIS/Focused Projects_IoTIS.pdf">Focused Projects in IoT and Intelligent System</a>
+                </li>
+              </ul>
+            </div><br>
     </card>
   </links-page>
 </template>
 <script>
+import axios from 'axios'
+import { genBackendURL, convertNewsfeed } from '@/common.js'
 import LinksPage from '@/components/LinksPage'
 import Card from '@/components/Card'
 // import CollapseList from '@/components/CollapseList'
@@ -16,11 +203,19 @@ export default {
   name: 'Iotis',
   data () {
     return {
-      
+      slides:{},
     }
   },
   created () {
-  	this.$emit('hideloader', true)
+  	axios.get(genBackendURL('activities/coecarousel'))
+         .then(response => {
+          let coecarousels = response.data.coecarousels
+          this.slides = coecarousels.IOTIS
+          this.$emit('hideloader', true)
+          })
+         .catch(e => {
+           console.log(e)
+         })
          
   },
   components: {
