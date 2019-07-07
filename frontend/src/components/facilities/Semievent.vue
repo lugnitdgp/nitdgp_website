076220e506567b4089062438c034cf1da1728aff
Compro-Prasad <comproprasad@gmail.com>
2019-07-07 20:41:39 +0530

    file name change in ecn

--- frontend/src/components/facilities/Semievent.vue
+++ frontend/src/components/facilities/Semievent.vue
@@ -18,7 +18,10 @@
               <a href="https://docs.google.com/forms/d/e/1FAIpQLScQHzEuNgty8UX-uaME7tRO4ymoJ1OKGfTdR9X_78Fi3lJgqA/viewform?usp=pp_url" target="_blank" style="color: #000;">Online Registration Link</a>
             </p>
             <p style="margin-top: 25px;background: #fff;width: 90%;margin-left: 4%;color: #000;">
-              <a href="http://nitdgp.ac.in/MU/Programme schedule_Final.pdf" target="_blank" style="color: #000;">Programme Schedule</a>
+              <a href="http://nitdgp.ac.in/MU/workshop_brochure.pdf" target="_blank" style="color: #000;">Workshop Brochure</a>
+            </p>
+            <p style="margin-top: 25px;background: #fff;width: 90%;margin-left: 4%;color: #000;">
+              <a href="http://nitdgp.ac.in/MU/Programme schedule_ecn.pdf" target="_blank" style="color: #000;">Programme Schedule</a>
             </p>
           </div>
         </div>
