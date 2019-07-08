5eea96298a2dda8507f860a1ae03ce217f7b26a9
Compro-Prasad <comproprasad@gmail.com>
2019-07-08 18:35:38 +0530

    few file added in Departmental promotion

--- frontend/src/components/information/Dpromotion.vue
+++ frontend/src/components/information/Dpromotion.vue
@@ -3,20 +3,27 @@
     <card title="Departmental Promotion">
       <ul class="list-group list-gr">
         <li>
-          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/DPC Notification 2019.pdf">Notification Regarding Departmental Promotion</a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/Circular_cancellation .pdf">Cancellation of Circular No. NITD/Estt./2301/2019 Date - 4th July 2019 </a>
         </li>
         <li>
-          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/Revised_Recruitment_Rules_for_NonTeaching_in_NITs_20122017.pdf">Recruitment Rules 2017 (Non-Teaching )</a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/reservation_roster2019.pdf">Reservation Roster of 31.03.2019 </a>
         </li>
         <li>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/Notification_updated.pdf">Notification Regarding Departmental Promotion</a>
+        </li>
+        <li>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/Recruitment_Rules_updated.pdf">Recruitment Rules 2017 (Non-Teaching )</a>
+        </li>
+        <!--<li>
           <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/Education Qualification DPC.pdf">Education Qualifications Requirment for the Employee Apointed Before 05.02.2014 </a>
-        </li>
+        </li>-->
         <li>
-          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/DPC Application Form.pdf">Application Form ( PDF ) </a>
+          <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/Application_form_updated.pdf">Application Form ( PDF ) </a>
         </li>
-        <li>
+        <!--<li>
           <a class="list-group-item" target="new" href="http://nitdgp.ac.in/AllPDF/DP/DPC Application Form.doc">Application Form ( DOC ) </a>
-        </li>
+        </li>-->
+        
       </ul>
     </card>
   </links-page>
