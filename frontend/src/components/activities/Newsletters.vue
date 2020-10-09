6b6be07f0c185ee9518f3f3a2e76fec166caa2cc
Server-Room <support@mail1.nitdgp.ac.in>
2020-10-09 13:23:28 +0530

    data added

--- frontend/src/components/activities/Newsletters.vue
+++ frontend/src/components/activities/Newsletters.vue
@@ -17,19 +17,23 @@ export default {
   name: "Newsletters",
   data () {
     return {
-      newslettersList: [],
+      newslettersList: [      
+		{title:"Newsletters 2019", file:"https://admin.nitdgp.ac.in/files/newsfeed/2020/10/08/Compiled_Newsletter_9_compressed.pdf",date: "2020-10-08"},
+		{title:"NIT Durgapur Newsletter - Special issue on COVID-19", file:"https://admin.nitdgp.ac.in/files/newsfeed/2020/08/04/COVID_Special_Newsletter.pdf",date: "2020-08-02"}
+	],
       paginate: ['newslettersList']
     }
   },
   created () {
-    axios.get(genBackendURL("activities/newsletters"))
-         .then(response => {
-           this.newslettersList = response.data.tenders
-           this.$emit('hideloader', true)
-         })
-         .catch(e => {
-           console.log(e)
-         })
+  	this.$emit('hideloader', true)
+    // axios.get(genBackendURL("activities/newsletters"))
+    //      .then(response => {
+    //        this.newslettersList = response.data.tenders
+    //        this.$emit('hideloader', true)
+    //      })
+    //      .catch(e => {
+    //        console.log(e)
+    //      })
   },
   components: {
     LinksPage,
