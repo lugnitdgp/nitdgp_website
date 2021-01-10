ba0d5c808170290cf328dc8c8b6b76d575a1f233
Server-Room <support@mail1.nitdgp.ac.in>
2021-01-10 13:35:06 +0530

    page modified

--- frontend/src/components/ConvocationGuest.vue
+++ frontend/src/components/ConvocationGuest.vue
@@ -11,13 +11,13 @@
             Dr. Ramesh Pokhriyal, Hon’ble Education Minister is also known by his pen name Nishank. He was born in Pinani village, Pauri Garhwal, Uttarakhand to Paramanand Pokhriyal and Vishambhari Devi. He received an M.A. degree from Hemwati Nandan Bahuguna Garhwal University. He represents the Haridwar Parliamentary constituency of Uttarakhand in the 17th Lok Sabha. He was the 5th Chief Minister of Uttarakhand from 2009 to 2011. He was member of 16th Lok Sabha and chairperson, Committee on Government Assurances. Pokhriyal has written novels, stories and poems. He has authored 44 books in Hindi, some of which have been translated to English as well as other Indian languages.
           </div>
         </div>
-        <div class="single-guest">
+        <div class="single-guest">          
+          <div class="details">
+            Prof. Ashutosh Sharma (Secretary, Department of Science and Technology, Govt. of India) is an Institute Chair Professor and C. V. Seshadri Chair Professor at the Department of chemical engineering of Indian Institute of Technology Kanpur. Prof. Ashutosh Sharma got his B.Tech degree in Chemical Engineering from Indian Institute of Technology Kanpur (IIT Kanpur), India in 1982. Prof. Sharma graduated with master's degree from Pennsylvania State University in 1984 and three years later obtained his Ph.D. in Chemical Engineering from the University at Buffalo. He is best known for his pioneering research work in the areas of colloids, thin film, interfaces, adhesion, patterning and in the fabrication and application of self-assembled nano-structures.
+          </div>
           <div class="imageview">
             <img src="http://nitdgp.ac.in/convocation16guest/ashutosh.jpg" class="img img-responsive img-thumbnail">
             <p class="text-center text-black">Guest of Honour, Prof. Ashutosh Sharma</p>
-          </div>
-          <div class="details">
-            Prof. Ashutosh Sharma (Secretary, Department of Science and Technology, Govt. of India) is an Institute Chair Professor and C. V. Seshadri Chair Professor at the Department of chemical engineering of Indian Institute of Technology Kanpur. Prof. Ashutosh Sharma got his B.Tech degree in Chemical Engineering from Indian Institute of Technology Kanpur (IIT Kanpur), India in 1982. Prof. Sharma graduated with master's degree from Pennsylvania State University in 1984 and three years later obtained his Ph.D. in Chemical Engineering from the University at Buffalo. He is best known for his pioneering research work in the areas of colloids, thin film, interfaces, adhesion, patterning and in the fabrication and application of self-assembled nano-structures.
           </div>
         </div>
         <div class="single-guest">
@@ -30,12 +30,12 @@
           </div>
         </div>
         <div class="single-guest">
+          <div class="details">
+            Prof. Anupam Basu, currently the Director and Professor of NIT Durgapur, is also a Professor at Department of Computer Science Engineering in IIT Kharagpur, India. Previously He has taught at University of California, Irvine and University of Guelph, Ontario, Canada as a visiting Professor and have been the Humboldt Fellow at TU Dortmund, Germany. Even before that, He has been a Systems Engineer for Webel Computers Ltd, India. His research Interests are Embedded Systems, Natural Language Processing, Assistive Technology, Natural Interface Design, Human Computer Interaction and ICT4D.
+          </div>
           <div class="imageview">
             <img src="http://nitdgp.ac.in/convocation16guest/Prof. Anupam Basu.jpg" class="img img-responsive img-thumbnail">
             <p class="text-center text-black">Director of NIT Durgapur, Prof. Anupam Basu</p>
-          </div>
-          <div class="details">
-            Prof. Anupam Basu, currently the Director and Professor of NIT Durgapur, is also a Professor at Department of Computer Science Engineering in IIT Kharagpur, India. Previously He has taught at University of California, Irvine and University of Guelph, Ontario, Canada as a visiting Professor and have been the Humboldt Fellow at TU Dortmund, Germany. Even before that, He has been a Systems Engineer for Webel Computers Ltd, India. His research Interests are Embedded Systems, Natural Language Processing, Assistive Technology, Natural Interface Design, Human Computer Interaction and ICT4D.
           </div>
         </div>
       </div>
@@ -96,8 +96,20 @@ export default {
     padding: 10px;
     margin-bottom: 8px;
   }
+  .single-guest:nth-child(1){
+    background: tomato;
+  }
+  .single-guest:nth-child(2){
+    background: #005cb9;
+  }
+  .single-guest:nth-child(3){
+    background: #736AFF;
+  }
+  .single-guest:nth-child(4){
+    background: #3090C7;
+  }
   .details{
-    margin-left: 15px;
+    margin: 8px;
     text-align: justify;
     width: 100%;
     color: #000;
@@ -108,10 +120,21 @@ export default {
       height:auto;
       width: 100%;
     } 
+    .imageview img{
+      width: 100%;
+      height: 100%;
+    }
+    .details{
+      margin-top: 38px;
+      text-align: justify;
+      width: 100%;
+      color: #000;
+      font-size: 20px;
+    }
     .single-guest{
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
   }
 </style>
